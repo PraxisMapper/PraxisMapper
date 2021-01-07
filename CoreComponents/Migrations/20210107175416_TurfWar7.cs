13eab7c521a285a38877d709b819db22b7f785f1
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-01-07 13:08:35 -0500

    Additional changes for TurfWar mode

---
+++ CoreComponents/Migrations/20210107175416_TurfWar7.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreComponents.Migrations
+{
+    public partial class TurfWar7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RecordedAt",
+                table: "TurfWarScoreRecords",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Repeating",
+                table: "TurfWarConfigs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "TurfWarConfigs",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecordedAt",
+                table: "TurfWarScoreRecords");
+
+            migrationBuilder.DropColumn(
+                name: "Repeating",
+                table: "TurfWarConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "TurfWarConfigs");
+        }
+    }
+}
