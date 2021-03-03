50f3051dd1cf4c6ac243dbb1f4d9b7ef381d2c51
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-03-03 15:41:29 -0500

    PTT layer slippy tiles should be faster now.

--- CoreComponents/PaintTown.cs
+++ CoreComponents/PaintTown.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CoreComponents.DbTables;
 
 namespace CoreComponents
 {
     public static class PaintTown
     {
-        public static string LearnCell8(int instanceID, string Cell8)
+        public static List<PaintTownEntry> LearnCell8(int instanceID, string Cell8)
         {
             //Which factions own which Cell10s nearby?
             var db = new PraxisContext();
             var cellData = db.PaintTownEntries.Where(t => t.PaintTownConfigId == instanceID && t.Cell8 == Cell8).ToList();
-            string results = ""; //Cell8 + "|";
-            foreach (var cell in cellData)
-                results += cell.Cell10 + "=" + cell.FactionId + "|";
-            return results;
+            return cellData;
         }
     }
 }
