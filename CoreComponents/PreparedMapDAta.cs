d65f62061d4474e1eb47de1a8442ad0e74547c58
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-03-19 01:02:06 -0400

    Changing AreaSize logic to use the perimeter of an area instead of its surface area. This fixes draw order sorting and should let the DB do its work correctly as well on an index, though that index is 3.6 GB for national data.

--- CoreComponents/PreparedMapDAta.cs
+++ CoreComponents/PreparedMapDAta.cs
@@ -2,11 +2,12 @@
 
 namespace CoreComponents
 {
-    public class PreparedMapData
-    {
-        //The Minimum stuff to use PreparedGeometry for intersection checks.
-        public long PreparedMapDataID;
-        public IPreparedGeometry place;
-        public int AreaTypeId;
-    }
+    //public class PreparedMapData
+    //{
+    //    //The Minimum stuff to use PreparedGeometry for intersection checks.
+    //    public long PreparedMapDataID;
+    //    public IPreparedGeometry place;
+    //    public int AreaTypeId;
+    //    public double AreaSize;
+    //}
 }
