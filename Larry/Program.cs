0a4bd20b4d048ed645131684adf3f6966030863d
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-07-23 10:24:30 -0400

    A couple extra idle game fields

--- Larry/Program.cs
+++ Larry/Program.cs
@@ -622,7 +622,7 @@ namespace Larry
             var neCorner = new OpenLocationCode(intersectCheck.EnvelopeInternal.MaxY, intersectCheck.EnvelopeInternal.MaxX);
             //insert default entries for a new player.
             sqliteDb.PlayerStats.Add(new PlayerStats() { timePlayed = 0, distanceWalked = 0, score = 0 });
-            sqliteDb.Bounds.Add(new Bounds() { EastBound = neCorner.Decode().EastLongitude, NorthBound = neCorner.Decode().NorthLatitude, SouthBound = swCorner.Decode().SouthLatitude, WestBound = swCorner.Decode().WestLongitude, commonCodeLetters = commonStart });
+            sqliteDb.Bounds.Add(new Bounds() { EastBound = neCorner.Decode().EastLongitude, NorthBound = neCorner.Decode().NorthLatitude, SouthBound = swCorner.Decode().SouthLatitude, WestBound = swCorner.Decode().WestLongitude, commonCodeLetters = commonStart, BestIdleCompletionTime = 0, LastPlayedOn =0, StartedCurrentIdleRun = 0 });
             sqliteDb.IdleStats.Add(new IdleStats() { emptySpacePerSecond =0, emptySpaceTotal=0, graveyardSpacePerSecond=0, graveyardSpaceTotal=0, natureReserveSpacePerSecond=0, natureReserveSpaceTotal=0, parkSpacePerSecond=0, parkSpaceTotal=0, touristSpacePerSecond=0, touristSpaceTotal=0, trailSpacePerSecond=0, trailSpaceTotal=0 });
             sqliteDb.SaveChanges();
 
