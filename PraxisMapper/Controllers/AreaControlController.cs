4d4e61b1e7a3edc782a3f6d90fed57aab7182b65
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-09-07 14:11:10 -0400

    Exposing more generic endpoints for maptiles

--- PraxisMapper/Controllers/AreaControlController.cs
+++ PraxisMapper/Controllers/AreaControlController.cs
@@ -90,8 +90,11 @@ namespace PraxisMapper.Controllers
         [HttpGet]
         [Route("/[controller]/DrawFactionModeCell8HighRes/{Cell8}/")]
         [Route("/[controller]/DrawFactionModeCell8/{Cell8}")]
-        public FileContentResult DrawFactionModeCell8(string Cell8, string styleSet = "mapTiles")
+        public FileContentResult DrawFactionModeMergedCell8(string Cell8, string styleSet = "mapTiles")
         {
+            //TODO: I think it's better to have the background tiles be shared across modes, and to
+            //regenerate the smaller, overlay tiles more often. This means that I need one endpoint
+            //for the background normal map tiles and one for the overlay, and the app needs to draw both in order.
             PerformanceTracker pt = new PerformanceTracker("DrawFactionModeCell8");
             //We will try to minimize rework done.
             var db = new PraxisContext();
@@ -140,6 +143,48 @@ namespace PraxisMapper.Controllers
             var output = MapTiles.LayerTiles(info2, baseMapTile.tileData, factionColorTile.tileData);
             pt.Stop(Cell8);
             return File(output, "image/png");
+        }
+
+        [Route("/[controller]/DrawAreaControlOverlay/{Cell8}")]
+        public FileContentResult DrawAreaControlOverlay(string Cell8)
+        {
+            //TODO: I think it's better to have the background tiles be shared across modes, and to
+            //regenerate the smaller, overlay tiles more often. This means that I need one endpoint
+            //for the background normal map tiles and one for the overlay, and the app needs to draw both in order.
+            PerformanceTracker pt = new PerformanceTracker("DrawAreaControlOverlay");
+            //We will try to minimize rework done.
+            var db = new PraxisContext();
+            System.Collections.Generic.List<StoredOsmElement> places = null;
+            GeoArea pluscode = OpenLocationCode.DecodeValid(Cell8);
+            var factionColorTile = db.MapTiles.Where(mt => mt.PlusCode == Cell8 && mt.resolutionScale == 11 && mt.styleSet == "teamColor").FirstOrDefault();
+            if (factionColorTile == null || factionColorTile.MapTileId == null || factionColorTile.ExpireOn < DateTime.Now)
+            {
+                //Draw this entry
+                //requires a list of colors to use, which might vary per app
+                GeoArea CellEightArea = OpenLocationCode.DecodeValid(Cell8);
+                var cellPoly = Converters.GeoAreaToPolygon(pluscode);
+                //if (places == null) //Don't download the data twice if we already have it, just reset the tags.
+                //places = GetPlaces(CellEightArea, skipTags: true); // , includeGenerated: Configuration.GetValue<bool>("generateAreas") TODO restore generated area logic.
+
+                ImageStats info = new ImageStats(pluscode, 160, 200); //Cell8 size TODO use DrawPlusCode here, add parameteres as needed
+                var ops = MapTiles.GetPaintOpsForCustomDataElements(Converters.GeoAreaToPolygon(CellEightArea), "teamColor", "teamColor", info);
+
+                var results = MapTiles.DrawAreaAtSize(info, ops, bgColor); //MapTiles.DrawMPAreaControlMapTile(info, places);
+                if (factionColorTile == null) //create a new entry
+                {
+                    factionColorTile = new MapTile() { PlusCode = Cell8, CreatedOn = DateTime.Now, styleSet = "teamColor", resolutionScale = 11, tileData = results, areaCovered = Converters.GeoAreaToPolygon(CellEightArea) };
+                    db.MapTiles.Add(factionColorTile);
+                }
+                else //update the existing entry.
+                {
+                    factionColorTile.tileData = results;
+                    factionColorTile.ExpireOn = DateTime.Now.AddYears(10);
+                    factionColorTile.CreatedOn = DateTime.Now;
+                }
+                db.SaveChanges();
+            }
+            pt.Stop(Cell8);
+            return File(factionColorTile.tileData, "image/png");
         }
 
         [HttpGet]
