f118636a97ee1e4a678aa3d8ee9ffe5471143d0a
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-09-14 10:22:10 -0400

    More changes for PrivacyId, starting internal documentation.

--- CoreComponents/GenericData.cs
+++ CoreComponents/GenericData.cs
@@ -3,10 +3,17 @@ using Google.OpenLocationCode;
 
 namespace CoreComponents
 {
-    //Handles read/write for the generic area/player data tables.
-    //TODO: set up existing games to use this structure instead of their own hard-coded serverside bits.
+    //Handles read/write for the generic area/player/global data tables.
     public static class GenericData
     {
+        /// <summary>
+        /// Saves a key/value pair to a given PlusCode. Will reject a pair containing a player's deviceId in the database.
+        /// </summary>
+        /// <param name="plusCode">A valid PlusCode, excluding the + symbol.</param>
+        /// <param name="key">The key to save to the database. Keys are unique, and you cannot have multiples of the same key.</param>
+        /// <param name="value">The value to save with the key.</param>
+        /// <param name="expiration">Set an optional date to indicate when to ignore the value saved.</param>
+        /// <returns>true if data was saved, false if data was not.</returns>
         public static bool SetPlusCodeData(string plusCode, string key, string value, DateTime? expiration = null)
         {
             var db = new PraxisContext();
@@ -29,6 +36,12 @@ namespace CoreComponents
             return db.SaveChanges() == 1;
         }
 
+        /// <summary>
+        /// Get the value from a key/value pair saved on a PlusCode. Expired values will not be sent over.
+        /// </summary>
+        /// <param name="plusCode">A valid PlusCode, excluding the + symbol.</param>
+        /// <param name="key">The key to load from the database on the PlusCode</param>
+        /// <returns>The value saved to the key, or an empty string if no key/value pair was found.</returns>
         public static string GetPlusCodeData(string plusCode, string key)
         {
             var db = new PraxisContext();
@@ -38,18 +51,26 @@ namespace CoreComponents
             return row.dataValue;
         }
 
-        public static bool SetStoredElementData(long elementId, string key, string value, DateTime? expiration = null)
+        /// <summary>
+        /// Saves a key/value pair to a given map element. Will reject a pair containing a player's deviceId in the database.
+        /// </summary>
+        /// <param name="elementId">the Guid exposed to clients to identify the map element.</param>
+        /// <param name="key">The key to save to the database for the map element.</param>
+        /// <param name="value">The value to save with the key.</param>
+        /// <param name="expiration">Set an optional date to indicate when to ignore the value saved.</param>
+        /// <returns>true if data was saved, false if data was not.</returns>
+        public static bool SetStoredElementData(Guid elementId, string key, string value, DateTime? expiration = null)
         {
             var db = new PraxisContext();
             if (db.PlayerData.Any(p => p.deviceID == key || p.deviceID == value))
                 return false;
             //An upsert command would be great here, but I dont think the entities do that.
-            var row = db.CustomDataOsmElements.Where(p => p.StoredOsmElementId == elementId && p.dataKey == key).FirstOrDefault();
+            var row = db.CustomDataOsmElements.Where(p => p.privacyId == elementId && p.dataKey == key).FirstOrDefault();
             if (row == null)
             {
                 row = new DbTables.CustomDataOsmElement();
                 row.dataKey = key;
-                row.StoredOsmElementId = elementId;
+                row.privacyId = elementId;
                 db.CustomDataOsmElements.Add(row);
             }
             if(expiration != null)
@@ -58,15 +79,27 @@ namespace CoreComponents
             return db.SaveChanges() == 1;
         }
 
-        public static string GetElementData(long elementId, string key)
+        /// <summary>
+        /// Get the value from a key/value pair saved on a map element. Expired entries will be ignored.
+        /// </summary>
+        /// <param name="elementId">the Guid exposed to clients to identify the map element.</param>
+        /// <param name="key">The key to load from the database. Keys are unique, and you cannot have multiples of the same key.</param>
+        /// <returns>The value saved to the key, or an empty string if no key/value pair was found.</returns>
+        public static string GetElementData(Guid elementId, string key)
         {
             var db = new PraxisContext();
-            var row = db.CustomDataOsmElements.Where(p => p.StoredOsmElementId == elementId && p.dataKey == key).FirstOrDefault();
+            var row = db.CustomDataOsmElements.Where(p => p.privacyId == elementId && p.dataKey == key).FirstOrDefault();
             if (row == null || row.expiration.GetValueOrDefault(DateTime.MaxValue) < DateTime.Now)
                 return "";
             return row.dataValue;
         }
 
+        /// <summary>
+        /// Get the value from a key/value pair saved on a player's deviceId. Expired entries will be ignored.
+        /// </summary>
+        /// <param name="playerId">the player-specific ID used. Expected to be a unique DeviceID to identify a phone, per that device's rules.</param>
+        /// <param name="key">The key to load data from for the playerId.</param>
+        /// <returns>The value saved to the key, or an empty string if no key/value pair was found.</returns>
         public static string GetPlayerData(string playerId, string key)
         {
             var db = new PraxisContext();
@@ -76,6 +109,14 @@ namespace CoreComponents
             return row.dataValue;
         }
 
+        /// <summary>
+        /// Saves a key/value pair to a given player's DeviceID. Will reject a pair containing a PlusCode or map element Id.
+        /// </summary>
+        /// <param name="playerId"></param>
+        /// <param name="key">The key to save to the database. Keys are unique, and you cannot have multiples of the same key.</param>
+        /// <param name="value">The value to save with the key.</param>
+        /// <param name="expiration">Set an optional date to indicate when to ignore the value saved.</param>
+        /// <returns>true if data was saved, false if data was not.</returns>
         public static bool SetPlayerData(string playerId, string key, string value, DateTime? expiration = null)
         {
             if (DataCheck.IsPlusCode(key) || DataCheck.IsPlusCode(value))
@@ -102,33 +143,58 @@ namespace CoreComponents
             return db.SaveChanges() == 1;
         }
 
-        //NOTE: i thought about having both functions return both tables's worth of data merged, but I think
-        //for performance reasons it'll be better to keep them separate and allow the actual game to do 
-        //the merging logic in those cases. Most data sets will be one or the other.
+        /// <summary>
+        /// Load all of the key/value pairs in a PlusCode, including pairs saved to a longer PlusCode. Expired entries will be ignored.
+        /// (EX: calling this with an 8 character PlusCode will load all contained 10 character PlusCodes key/value pairs)
+        /// </summary>
+        /// <param name="plusCode">A valid PlusCode, excluding the + symbol.</param>
+        /// <returns>a List of results with the PlusCode, keys, and values</returns>
         public static List<CustomDataResult> GetAllDataInPlusCode(string plusCode) //TODO: add optional key filter
         {
             var db = new PraxisContext();
             var plusCodeArea = OpenLocationCode.DecodeValid(plusCode);
             var plusCodePoly = Converters.GeoAreaToPolygon(plusCodeArea);
-            var plusCodeData = db.CustomDataPlusCodes.Where(d => plusCodePoly.Intersects(d.geoAreaIndex)).Select(d => new CustomDataResult(d.PlusCode, d.dataKey, d.dataValue)).ToList();
+            var plusCodeData = db.CustomDataPlusCodes.Where(d => plusCodePoly.Intersects(d.geoAreaIndex))
+                .ToList() //Required to run the next Where on the C# side
+                .Where(row => row.expiration.GetValueOrDefault(DateTime.MaxValue) < DateTime.Now)
+                .Select(d => new CustomDataResult(d.PlusCode, d.dataKey, d.dataValue))
+                .ToList();
 
             return plusCodeData;
         }
 
+        /// <summary>
+        /// Load all of the key/value pairs in a GeoArea attached to a PlusCode. Expired entries will be ignored.
+        /// </summary>
+        /// <param name="area">the GeoArea to pull data for.</param>
+        /// <returns>a List of results with the PlusCode, keys, and values</returns>
         public static List<CustomDataResult> GetAllPlusCodeDataInArea(GeoArea area) //TODO: add optional key filter
         {
             var db = new PraxisContext();
             var poly = Converters.GeoAreaToPolygon(area);
-            var plusCodeData = db.CustomDataPlusCodes.Where(d => poly.Intersects(d.geoAreaIndex)).Select(d => new CustomDataResult(d.PlusCode, d.dataKey, d.dataValue)).ToList();
+            var plusCodeData = db.CustomDataPlusCodes.Where(d => poly.Intersects(d.geoAreaIndex))
+                .ToList() //Required to run the next Where on the C# side
+                .Where(row => row.expiration.GetValueOrDefault(DateTime.MaxValue) < DateTime.Now)
+                .Select(d => new CustomDataResult(d.PlusCode, d.dataKey, d.dataValue))
+                .ToList();
 
             return plusCodeData;
         }
 
+        /// <summary>
+        /// Load all of the key/value pairs in a GeoArea attached to a map element. Expired entries will be ignored.
+        /// </summary>
+        /// <param name="area">the GeoArea to pull data for.</param>
+        /// <returns>a List of results with the map element ID, keys, and values</returns>
         public static List<CustomDataAreaResult> GetAllDataInArea(GeoArea area)
         {
             var db = new PraxisContext();
             var poly = Converters.GeoAreaToPolygon(area);
-            var data = db.CustomDataOsmElements.Where(d => poly.Intersects(d.storedOsmElement.elementGeometry)).Select(d => new CustomDataAreaResult(d.StoredOsmElementId, d.dataKey, d.dataValue)).ToList();
+            var data = db.CustomDataOsmElements.Where(d => poly.Intersects(d.storedOsmElement.elementGeometry))
+                .ToList() //Required to run the next Where on the C# side
+                .Where(row => row.expiration.GetValueOrDefault(DateTime.MaxValue) < DateTime.Now)
+                .Select(d => new CustomDataAreaResult(d.privacyId, d.dataKey, d.dataValue))
+                .ToList();
 
             return data;
         }
@@ -142,15 +208,29 @@ namespace CoreComponents
         //    return data;
         //}
 
-        public static List<CustomDataAreaResult> GetAllDataInPlace(Guid elementId, int elementType)
+        /// <summary>
+        /// Load all of the key/value pairs attached to a map element. Expired entries will be ignored.
+        /// </summary>
+        /// <param name="elementId">the Guid exposed to clients to identify the map element.</param>
+        /// <returns>a List of results with the map element ID, keys, and values</returns>
+        public static List<CustomDataAreaResult> GetAllDataInPlace(Guid elementId)
         {
             var db = new PraxisContext();
-            var place = db.StoredOsmElements.Where(s => s.privacyId == elementId && s.sourceItemType == elementType).First();
-            var data = db.CustomDataOsmElements.Where(d => place.elementGeometry.Intersects(d.storedOsmElement.elementGeometry)).Select(d => new CustomDataAreaResult(d.StoredOsmElementId, d.dataKey, d.dataValue)).ToList();
+            var place = db.StoredOsmElements.Where(s => s.privacyId == elementId).First();
+            var data = db.CustomDataOsmElements.Where(d => d.storedOsmElement.elementGeometry.Intersects(d.storedOsmElement.elementGeometry))
+                .ToList() //Required to run the next Where on the C# side
+                .Where(row => row.expiration.GetValueOrDefault(DateTime.MaxValue) < DateTime.Now)
+                .Select(d => new CustomDataAreaResult(d.privacyId, d.dataKey, d.dataValue))
+                .ToList();
 
             return data;
         }
 
+        /// <summary>
+        /// Loads a key/value pair from the database that isn't attached to anything specific. Global entries do not expire.
+        /// </summary>
+        /// <param name="key">The key to load data from.</param>
+        /// <returns>The value saved to the key, or an empty string if no key/value pair was found.</returns>
         public static string GetGlobalData(string key)
         {
             var db = new PraxisContext();
@@ -161,6 +241,12 @@ namespace CoreComponents
             return row.dataValue;
         }
 
+        /// <summary>
+        /// Saves a key/value pair to the database that isn't attached to anything specific. Wil reject a pair that contains a player's device ID, PlusCode, or a map element ID. Global entries cannot be set to expire.
+        /// </summary>
+        /// <param name="key">The key to save to the database. Keys are unique, and you cannot have multiples of the same key.</param>
+        /// <param name="value">The value to save with the key.</param>
+        /// <returns>true if data was saved, false if data was not.</returns>
         public static bool SetGlobalData(string key, string value)
         {
             bool trackingPlayer = false;
