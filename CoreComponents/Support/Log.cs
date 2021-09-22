45507d1cc9ab26a399029dc3aa89a6d7b32b376d
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-09-22 10:03:17 -0400

    Logging should write to files faster

--- CoreComponents/Support/Log.cs
+++ CoreComponents/Support/Log.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PraxisCore
 {
@@ -6,7 +7,14 @@ namespace PraxisCore
     {
         //TODO: make this a service? Replace with default logging class?
         static string filename = "PraxisCore-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+        static StreamWriter sw = new StreamWriter(filename);
+        /// <summary>
+        /// The level of logging to display. Ignores messages if their output level is higher than this.
+        /// </summary>
         public static VerbosityLevels Verbosity = VerbosityLevels.Normal;
+        /// <summary>
+        /// If true, writes all console output to a file as well.
+        /// </summary>
         public static bool WriteToFile = false;
 
         private static object fileLock = new object();
@@ -18,6 +26,11 @@ namespace PraxisCore
                 //TODO: add ErrorsOnly, between Off and Normal?
         }
 
+        /// <summary>
+        /// Write a log message to the console, and a file if WriteToFile is true
+        /// </summary>
+        /// <param name="message">The string to write to the console/logfile</param>
+        /// <param name="outputLevel">What level of alert this log is. Will not be displayed/written if Verbosity is lower than this value.</param>
         public static void WriteLog(string message, VerbosityLevels outputLevel = VerbosityLevels.Normal)
         {
             if ((int)Verbosity < (int)outputLevel)
@@ -25,8 +38,8 @@ namespace PraxisCore
 
             Console.WriteLine(message);
             if (WriteToFile)
-                lock(fileLock) 
-                    System.IO.File.AppendAllText(filename, message + Environment.NewLine);
+                lock (fileLock)
+                    sw.WriteLine(message);
         }
 
     }
