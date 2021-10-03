317f29a3ab0f7eec394620814d10ff0c8d363619
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-10-03 19:40:08 -0400

    First working prototype for data editor. Lists and change global data values

---
+++ PraxisMapper/Models/EditData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace PraxisMapper.Models
+{
+    public class EditData
+    {
+        //The model for the AdminView/EditData page
+        public string accessKey;
+        public string currentGlobalKey;
+        public List<SelectListItem> globalDataKeys;
+    }
+}
