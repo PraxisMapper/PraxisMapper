3667f0436e60d4dafc73176c4d8a24656eb32df1
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-10-08 16:10:14 -0400

    Secure data endpoints enabled on the webserver

--- PraxisMapper/Controllers/SecureDataController.cs
+++ PraxisMapper/Controllers/SecureDataController.cs
@@ -26,6 +26,55 @@ namespace PraxisMapper.Controllers
             cache = memoryCacheSingleton;
         }
 
+        [HttpGet]
+        [Route("/[controller]/SetSecureElementData/{elementId}/{key}/{value}/{password}")]
+        [Route("/[controller]/SetSecureElementData/{elementId}/{key}/{value}/{password}/{expiresIn}")]
+        public bool SetSecureElementData(Guid elementId, string key, string value,string password, double? expiresIn = null)
+        {
+            return GenericData.SetSecureElementData(elementId, key, value, password, expiresIn);
+        }
+
+        [HttpGet]
+        [Route("/[controller]/GetSecureElementData/{elementId}/{key}/{password}")]
+        public string GetSecureElementData(Guid elementId, string key, string password)
+        {
+            return GenericData.GetSecureElementData(elementId, key, password);
+        }
+
+        [HttpGet]
+        [Route("/[controller]/SetSecurePlayerData/{deviceId}/{key}/{value}/{password}")]
+        [Route("/[controller]/SetSecurePlayerData/{deviceId}/{key}/{value}/{password}/{expiresIn}")]
+        public bool SetSecurePlayerData(string deviceId, string key, string value, string password, double? expiresIn = null)
+        {
+            return GenericData.SetSecurePlayerData(deviceId, key, value, password, expiresIn);
+        }
+
+        [HttpGet]
+        [Route("/[controller]/GetSecurePlayerData/{deviceId}/{key}/{password}")]
+        public string GetSecurePlayerData(string deviceId, string key, string password)
+        {
+            return GenericData.GetSecurePlayerData(deviceId, key, password);
+        }
+
+        [HttpGet]
+        [Route("/[controller]/SetSecurePlusCodeData/{plusCode}/{key}/{value}/{password}")]
+        [Route("/[controller]/SetSecurePlusCodeData/{plusCode}/{key}/{value}/{password}/{expiresIn}")]
+        public bool SetSecurePlusCodeData(string plusCode, string key, string value, string password, double? expiresIn = null)
+        {
+            if (!DataCheck.IsInBounds(cache.Get<IPreparedGeometry>("serverBounds"), OpenLocationCode.DecodeValid(plusCode)))
+                return false;
+            return GenericData.SetSecurePlusCodeData(plusCode, key, value, password, expiresIn);
+        }
+
+        [HttpGet]
+        [Route("/[controller]/GetSecurePlusCodeData/{plusCode}/{key}/{password}")]
+        public string GetSecurePlusCodeData(string plusCode, string key, string password)
+        {
+            if (!DataCheck.IsInBounds(cache.Get<IPreparedGeometry>("serverBounds"), OpenLocationCode.DecodeValid(plusCode)))
+                return "";
+            return GenericData.GetSecurePlusCodeData(plusCode, key, password);
+        }
+
 
         //Reminder: BCrypt is for passwords, and is a one-way hash. This can't retreive an unknown value. Need a symmetric crypto plan for that.
 
