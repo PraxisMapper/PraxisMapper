300ca7563c7afa73a6178101ffff7e688cbe6816
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-10-16 14:05:45 -0400

    Partial progress on style editor.

---
+++ PraxisMapper/Controllers/StyleDataController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PraxisCore;
+using System.Linq;
+
+namespace PraxisMapper.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StyleDataController : Controller
+    {
+        [HttpGet]
+        [Route("/[controller]/GetStyleSetEntryNames/{styleSet}")]
+        public string GetStyleSetEntryNames(string styleSet)
+        {
+            var db = new PraxisContext();
+            var data = db.TagParserEntries.Where(t => t.styleSet == styleSet).OrderBy(t => t.MatchOrder).Select(t => t.name).ToList();
+            return string.Join('|', data);
+        }
+
+        //These 2 functions do need to use IDs in case names change.
+        [HttpGet]
+        [Route("/[controller]/GetStyleSetEntryValues/{styleSet}/{entryName}")]
+        public string GetStyleSetEntryValues(string styleSet, string entryName)
+        {
+            var db = new PraxisContext();
+            var data = db.TagParserEntries.FirstOrDefault(t => t.styleSet == styleSet && t.name == entryName);
+            return data.MatchOrder + "|" + data.name + "|" + data.IsGameElement + "|" + data.minDrawRes + "|" + data.maxDrawRes;
+        }
+
+        [HttpGet]
+        [Route("/[controller]/UpdateStyleSetEntryValues/{styleSet}/{matchOrder}/{entryName}/{isGameElement}/{minDrawRes}/{maxDrawRes}")]
+        public void UpdateStyleSetEntryValues(string styleSet, int matchOrder, string entryName, bool isGameElement, double minDrawRes, double maxDrawRes)
+        {
+            //Hasnt yet been tested.
+            var db = new PraxisContext();
+            var data = db.TagParserEntries.FirstOrDefault(t => t.styleSet == styleSet && t.name == entryName);
+            data.MatchOrder = matchOrder;
+            data.name = entryName;
+            data.IsGameElement = isGameElement;
+            data.minDrawRes = minDrawRes;
+            data.maxDrawRes = maxDrawRes;
+            db.SaveChanges();
+        }
+    }
+}
