96466178c2460310b5dfd821365bfee325523387
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2022-02-28 11:02:35 -0500

    Minor cleanup changes

--- CoreComponents/Support/Records.cs
+++ CoreComponents/Support/Records.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using NetTopologySuite;
-using NetTopologySuite.Geometries;
+﻿using NetTopologySuite.Geometries;
+using System;
 using static PraxisCore.DbTables;
 
 namespace PraxisCore.Support
 {
     //records are new C# 9.0 shorthand for an immutable class (only edited on creation).
-    public record MapDataForJson(string name, string place, string type, long? WayId, long? NodeId, long? RelationId, int AreaTypeId, double? AreaSize); //used for serializing MapData, since Geography types do not serialize nicely.
-
     public record CoordPair(float lat, float lon); //Used only in TestPerfApp
-
-    public record StoredOsmElementForJson(long id, long sourceItemID, int sourceItemType, string elementGeometry, string WayTags, bool IsGameElement, bool isUserProvided, bool isGenerated);
-
     public record CompletePaintOp(Geometry elementGeometry, double areaSize, TagParserPaint paintOp, string tagValue, double lineWidth);
-
     public record CustomDataResult(string plusCode, string key, string value);
     public record CustomDataAreaResult(Guid elementId, string key, string value);
-
     public record CustomDataPlayerResult(string deviceId, string key, string value);
 
 }
