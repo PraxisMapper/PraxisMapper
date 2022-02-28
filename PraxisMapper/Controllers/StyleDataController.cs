1594a9bec7676e6de0ffb9d8aefd8123978ae26d
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2022-02-28 13:23:48 -0500

    Server can now relay style data to a client (if a client draws maptiles itself)

--- PraxisMapper/Controllers/StyleDataController.cs
+++ PraxisMapper/Controllers/StyleDataController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PraxisCore;
 using System.Linq;
 
@@ -25,6 +26,29 @@ namespace PraxisMapper.Controllers
             var db = new PraxisContext();
             var data = db.TagParserEntries.FirstOrDefault(t => t.styleSet == styleSet && t.name == entryName);
             return data.MatchOrder + "|" + data.name + "|" + data.IsGameElement + "|" + data.minDrawRes + "|" + data.maxDrawRes + "|" + data.id;
+        }
+
+        [HttpGet]
+        [Route("/[controller]/json/{styleSet}/")]
+        public JsonResult GetStyleJson(string styleSet)
+        {
+            var db = new PraxisContext();
+            JsonOptions jo = new JsonOptions();
+            jo.JsonSerializerOptions.IncludeFields = true;
+            jo.JsonSerializerOptions.MaxDepth = 2;
+            var data = db.TagParserEntries.Include("TagParserMatchRules").Include("paintOperations").Where(t => t.styleSet == styleSet).ToList();
+            var returnData = data.Select(x => new { 
+                x.id, 
+                x.name, 
+                x.minDrawRes, 
+                x.maxDrawRes, 
+                x.styleSet, 
+                x.IsGameElement, 
+                x.MatchOrder, 
+                paintOperations = x.paintOperations.Select(po => new { po.fileName, po.FillOrStroke, po.fromTag, po.HtmlColorCode, po.id, po.layerId, po.LinePattern, po.LineWidth, po.maxDrawRes, po.minDrawRes, po.randomize }).ToList(),
+                TagParserMatchRules = x.TagParserMatchRules.Select(mr => new { mr.id, mr.Key, mr.MatchType, mr.Value}).ToList()  
+            });
+            return Json(returnData);
         }
 
         [HttpGet]
