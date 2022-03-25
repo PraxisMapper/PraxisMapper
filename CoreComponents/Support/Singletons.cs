41f9beedab57b7140ea5bbf16a9f3507941d2ce5
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2022-03-25 11:01:42 -0400

    Added new style to display only suggested gameplay elements as an overlay

--- CoreComponents/Support/Singletons.cs
+++ CoreComponents/Support/Singletons.cs
@@ -517,7 +517,9 @@ namespace PraxisCore
                     new StyleMatchRule() {Key = "*", Value = "*", MatchType = "default"},
             }},
 
-            //Paint the Town special style.
+            //
+            //Paint the Town style, uses fromTag.
+            //
             new StyleEntry() { MatchOrder = 1, Name ="tag",  StyleSet = "paintTown",
                 PaintOperations = new List<StylePaint>() {
                     new StylePaint() { HtmlColorCode = "01000000", FillOrStroke = "fill", LineWidthDegrees=0.00000625F, LinePattern= "solid", LayerId = 100, FromTag=true }
@@ -534,7 +536,9 @@ namespace PraxisCore
                     new StyleMatchRule() {Key = "*", Value = "*", MatchType = "default"},
             }},
 
+            //
             //new style to allow only outlines of ALL entries.
+            //
             new StyleEntry() { MatchOrder = 1, Name ="1",  StyleSet = "outlines",
                 PaintOperations = new List<StylePaint>() {
                     new StylePaint() { HtmlColorCode = "000000", FillOrStroke = "stroke", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 }
@@ -552,6 +556,133 @@ namespace PraxisCore
             }},
             //this name needs to exist because of the default style using this name.
             new StyleEntry() { MatchOrder = 10001, Name ="unmatched",  StyleSet = "outlines",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "00000000", FillOrStroke = "fill", LineWidthDegrees=0.00000625F, LinePattern= "solid", LayerId = 100 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "a", Value = "s", MatchType = "equals" }}
+            },
+
+            //
+            // Overlay style to show areas that PraxisMapper thinks are good places for gameplay.
+            // Includes generated areas.
+            //
+            new StyleEntry() { IsGameElement = true,  MatchOrder = 1, Name ="water", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CC0062FF", FillOrStroke = "fill", LineWidthDegrees=0.0000625F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CC0000FF", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key = "natural", Value = "water|strait|bay|coastline", MatchType = "or"},
+                    new StyleMatchRule() {Key = "waterway", Value ="*", MatchType="or" },
+                    new StyleMatchRule() {Key = "landuse", Value ="basin", MatchType="or" },
+                    new StyleMatchRule() {Key = "leisure", Value ="swimming_pool", MatchType="or" },
+                    new StyleMatchRule() {Key = "place", Value ="sea", MatchType="or" }, //stupid Labrador sea value.
+                }},
+            new StyleEntry() { IsGameElement = true, MatchOrder = 2, Name ="wetland", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CC609124", FillOrStroke = "fill", LineWidthDegrees=0.0000625F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CC289124", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                 StyleMatchRules = new List<StyleMatchRule>() {
+                     new StyleMatchRule() { Key = "natural", Value = "wetland", MatchType = "equals" }
+                 }},
+            new StyleEntry() { IsGameElement = true, MatchOrder = 3, Name ="park", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+
+                    new StylePaint() { HtmlColorCode = "CC93FF61", FillOrStroke = "fill", LineWidthDegrees=0.0000625F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CCB2FF8F", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "leisure", Value = "park", MatchType = "or" },
+                    new StyleMatchRule() { Key = "leisure", Value = "playground", MatchType = "or" },
+            }},
+            new StyleEntry() { IsGameElement = true, MatchOrder = 4, Name ="beach", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CCF9FF8F", FillOrStroke = "fill", LineWidthDegrees=0.0000625F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CCFFEA8F", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key = "natural", Value = "beach|shoal", MatchType = "or" },
+                    new StyleMatchRule() {Key = "leisure", Value="beach_resort", MatchType="or"}
+            } },
+            new StyleEntry() { IsGameElement = true, MatchOrder = 5, Name ="university", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CCFFFFE5", FillOrStroke = "fill", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CCF5EED3", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "amenity", Value = "university|college", MatchType = "any" }}
+            },
+            new StyleEntry() { IsGameElement = true, MatchOrder = 6, Name ="natureReserve", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CC124504", FillOrStroke = "fill", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CC027021", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "leisure", Value = "nature_reserve", MatchType = "equals" }}
+            },
+            new StyleEntry() {IsGameElement = true, MatchOrder = 7, Name ="cemetery", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CCAACBAF", FillOrStroke = "fill", FileName="Landuse-cemetery.png", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CC404040", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "landuse", Value = "cemetery", MatchType = "or" },
+                    new StyleMatchRule() {Key="amenity", Value="grave_yard", MatchType="or" } }
+            },
+            new StyleEntry() { IsGameElement = true, MatchOrder = 8, Name ="retail", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CCF2BBE9", FillOrStroke = "fill", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CCF595E5", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "landuse", Value = "retail|commercial", MatchType = "or"},
+                    new StyleMatchRule() {Key="building", Value="retail|commercial", MatchType="or" },
+                    new StyleMatchRule() {Key="shop", Value="*", MatchType="or" }
+            }},
+            new StyleEntry() { IsGameElement = true, MatchOrder = 9, Name ="tourism", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CC660033", FillOrStroke = "fill", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CCFF0066", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "tourism", Value = "*", MatchType = "equals" }}
+            },
+            new StyleEntry() { IsGameElement = true, MatchOrder = 10, Name ="historical", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CCB3B3B3", FillOrStroke = "fill", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CC9D9D9D", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "historic", Value = "*", MatchType = "equals" }}
+            },
+            new StyleEntry() { IsGameElement = true, MatchOrder = 12, Name ="trail", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CC7A5206", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 100 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key="highway", Value="path|bridleway|cycleway|footway|living_street", MatchType="any"},
+                    new StyleMatchRule() { Key="footway", Value="sidewalk|crossing", MatchType="not"}
+            }},
+            new StyleEntry() { IsGameElement = true, MatchOrder = 13, Name ="serverGenerated", StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "CC76E3E1", FillOrStroke = "fill", LineWidthDegrees=0.0000625F, LinePattern= "solid", LayerId = 100 },
+                    new StylePaint() { HtmlColorCode = "CC5CB5B4", FillOrStroke = "stroke", LineWidthDegrees=0.0001875F, LinePattern= "solid", LayerId = 99 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key="generated", Value="praxisMapper", MatchType="equals"},
+            }},
+            //background is a mandatory style entry name, but its transparent here..
+            new StyleEntry() { MatchOrder = 10000, Name ="background",  StyleSet = "suggestedGameplay",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "00000000", FillOrStroke = "fill", LineWidthDegrees=0.00000625F, LinePattern= "solid", LayerId = 101 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key = "bg", Value = "bg", MatchType = "equals"}, //this one only gets called by name anyways.
+            }},
+            //this name needs to exist because of the default style using this name.
+            new StyleEntry() { MatchOrder = 10001, Name ="unmatched",  StyleSet = "suggestedGameplay",
                 PaintOperations = new List<StylePaint>() {
                     new StylePaint() { HtmlColorCode = "00000000", FillOrStroke = "fill", LineWidthDegrees=0.00000625F, LinePattern= "solid", LayerId = 100 }
                 },
