db096c87a43f81c4b9309b976621e7e62cf8c9a5
drakewill <drake@drakewill-crl>
2022-05-19 22:06:30 -0400

    Added support for a maintenance message that locks out all endpoints and returns a single error message.

---
+++ PraxisMapper/Classes/PraxisMaintenanceMessage.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraxisMapper.Classes;
+//NOTE: PraxiHeaderCheck is intended as a simple app guard, that doesn't require logging in. 
+//If you have an authentication system in place, or use the PraxisAuthentication middleware,
+//this probably does no additional help.
+public class PraxisMaintenanceMessage
+{
+    //Define target endpoints to protect, so webview apps will load without issues.
+    public static string outputMessage;
+
+    public PraxisMaintenanceMessage(RequestDelegate next)
+    {
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var response = context.Response;
+        response.ContentType = "text/text";
+        response.StatusCode = 500;
+        await response.WriteAsync(outputMessage);
+    }
+}
+
+public static class PraxisMaintenanceMessageExtensions
+{
+    public static IApplicationBuilder UsePraxisMaintenanceMessage(this IApplicationBuilder builder, string message)
+    {
+        PraxisMaintenanceMessage.outputMessage = message;
+        return builder.UseMiddleware<PraxisMaintenanceMessage>();
+    }
+}
