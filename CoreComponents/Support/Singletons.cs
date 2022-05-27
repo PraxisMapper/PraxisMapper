919b9d298bf96a36a2c62297341015782fa7a205
drakewill <drake@drakewill-crl>
2022-05-27 15:12:20 -0400

    Comment

--- CoreComponents/Support/Singletons.cs
+++ CoreComponents/Support/Singletons.cs
@@ -56,7 +56,7 @@ namespace PraxisCore
             //So I will re-order this to handle tertiary first, then the gameElements that might be buildings, then plain buildings, and the remaining elements after that.
             //This should mean that 60%+ of elements match in 6 checks or less.
             //MapTiles: Roads of varying sizes and colors to match OSM colors
-            new StyleEntry() { MatchOrder = 10, Name ="tertiary", StyleSet = "mapTiles", //This is MatchOrde 1 because
+            new StyleEntry() { MatchOrder = 10, Name ="tertiary", StyleSet = "mapTiles", //This is MatchOrde 1 because its one of the most common entries, is the correct answer 30$ of the time.
                 PaintOperations = new List<StylePaint>() {
                     new StylePaint() { HtmlColorCode = "ffffff", FillOrStroke = "stroke", LineWidthDegrees=0.0000125F, LinePattern= "solid", LayerId = 98, MaxDrawRes = ConstantValues.zoom10DegPerPixelX / 2},
                     new StylePaint() { HtmlColorCode = "8f8f8f", FillOrStroke = "stroke", LineWidthDegrees=0.0000375F, LinePattern= "solid", LayerId = 99, MaxDrawRes = ConstantValues.zoom10DegPerPixelX / 2}
