8a779b579b8bab949b85b7d7505a98e428c22a3f
drakewill <drake@drakewill-crl>
2022-08-17 20:47:44 -0400

    Version plugin behaves as expected

--- PraxisVersionPlugin/Controllers/VersionController.cs
+++ PraxisVersionPlugin/Controllers/VersionController.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using PraxisCore;
+using PraxisCore.Support;
 
 namespace PraxisVersionPlugin.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class VersionController : Controller
+    public class VersionController : Controller, IPraxisPlugin
     {
         [HttpGet]
-        [Route("[controller]")]
+        [Route("/[controller]")]
+        [Route("/[controller]/Index")]
         public string GetVersion()
         {
             return GenericData.GetGlobalData("clientVersion").ToUTF8String();
