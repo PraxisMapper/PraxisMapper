dafc8d1c7f63d675c56c79950fba1483aff8a4b1
PraxisMapper <praxismapper@gmail.com>
2023-01-06 22:59:54 -0500

    forward progress on the mastodon plugin

---
+++ PraxisMastodonPlugin/MastodonGlobals.cs
@@ -0,0 +1,17 @@
+﻿namespace PraxisMastodonPlugin
+{
+
+    public class MastodonPost
+    {
+        public Guid id;
+        public string contents;
+    }
+
+    
+public static class MastodonGlobals
+    {
+        public static List<string> followers = new List<string>();
+        public static List<string> posts = new List<string>();
+
+    }
+}
---
+++ PraxisMastodonPlugin/MastodonStartup.cs
@@ -0,0 +1,26 @@
+﻿using PraxisCore.Support;
+using PraxisMapper.Classes;
+
+namespace PraxisMastodonPlugin
+{
+    public class MastodonStartup : IPraxisStartup
+    {
+        static bool initialized = false;
+        public static void Startup()
+        {
+            if (initialized)
+                return;
+
+            //add mastodon endpoints to access whitelist
+            PraxisAuthentication.whitelistedPaths.Add("/.well-known/webfinger");
+            PraxisAuthentication.whitelistedPaths.Add("/announcements"); //should cover all sub-paths.
+
+            //Set Followers list
+
+            //Set OUtbox contents.
+
+
+
+        }
+    }
+}
