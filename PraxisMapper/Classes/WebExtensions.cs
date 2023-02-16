bc3841307379dbb72c92a762c15d4119e7008189
PraxisMapper <praxismapper@gmail.com>
2023-02-16 00:44:46 -0500

    Add a ReadBody helper and extensions

--- PraxisMapper/Classes/WebExtensions.cs
+++ PraxisMapper/Classes/WebExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using PraxisCore;
 using System.Linq;
 using System.Net;
 
@@ -12,6 +13,16 @@ namespace PraxisMapper.Classes
             IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
 
             return hostIPs.Any(h => IPAddress.IsLoopback(h) || localIPs.Any(l => h.Equals(l)));
+        }
+
+        public static string ReadBody(this HttpRequest r)
+        {
+            return GenericData.ReadBody(r.BodyReader, r.ContentLength).ToUTF8String();
+        }
+
+        public static T ReadBody<T>(this HttpRequest r)
+        {
+            return GenericData.ReadBody(r.BodyReader, r.ContentLength).ToUTF8String().FromJsonTo<T>();
         }
     }
 }
