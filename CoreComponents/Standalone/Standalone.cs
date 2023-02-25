b90689f04509e83899881e39b63c54bde6d4dbb4
PraxisMapper <praxismapper@gmail.com>
2023-02-25 13:50:24 -0500

    standalone updates

--- CoreComponents/Standalone/Standalone.cs
+++ CoreComponents/Standalone/Standalone.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using static PraxisCore.DbTables;
-using static PraxisCore.StandaloneDbTables;
+using static PraxisCore.Standalone.StandaloneDbTables;
 using static PraxisCore.ConstantValues;
 using System.Collections.Concurrent;
 
