883117088b4b500931fbe8feec065f2d078e08db
PraxisMapper <PraxisMapper@gmail.com>
2023-03-07 13:29:06 -0500

    Remove classes that are no longer used

--- CoreComponents/Support/Records.cs
+++ CoreComponents/Support/Records.cs
@@ -9,9 +9,6 @@ namespace PraxisCore.Support
     //records are new C# 9.0 shorthand for an immutable class (only edited on creation). C# 10 adds record structs, which are value types instead of reference types and a bit faster in some cases..
     public record struct CoordPair(float lat, float lon); //Used only in TestPerfApp
     public record struct CompletePaintOp(Geometry elementGeometry, double drawSizeHint, StylePaint paintOp, string tagValue, double lineWidthPixels);
-    public record struct CustomDataAreaResult(string plusCode, string key, string value);
-    public record struct CustomDataPlaceResult(Guid elementId, string key, string value);
-    public record struct CustomDataPlayerResult(string accountId, string key, string value);
     public readonly record struct AreaInfo(string name, string style, Guid privacyId);
     public readonly record struct AreaDetail(string plusCode, AreaInfo data);
     public readonly record struct AreaDetailAll(string plusCode, List<AreaInfo> data);
