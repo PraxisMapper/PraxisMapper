919504d0f2d4c82f259ad92b17341dd2ad18f2e2
PraxisMapper <praxismapper@gmail.com>
2023-03-12 13:35:58 -0400

    More GameTools fixes and optimizations

--- CoreComponents/GameTools/RecentActivityTracker.cs
+++ CoreComponents/GameTools/RecentActivityTracker.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace PraxisCore.GameTools
 {
-    public class RecentActivityTracker
-    {
+    public class RecentActivityTracker {
         //Stores Cell10s a player has entered within the last $hourDelay, with a datestamp for when that entry will be removed
         public int hourDelay { get; set; } = 22;
-        public Dictionary<string, DateTime> history { get; set; } = new Dictionary<string, DateTime>();
+        public Dictionary<string, DateTime> history { get; set; } = new Dictionary<string, DateTime>(); //Holds the expiration time of the given PlusCode. (EX: if an entry is present, it is not recent)
+        public DateTime nextPurge { get; set; } = DateTime.UtcNow;
 
         /// <summary>
         /// Determines if the player has recently entered this PlusCode. Returns 'true' if this is the first time in the last $hourDelay they have entered this Cell10.
@@ -28,7 +28,11 @@ namespace PraxisCore.GameTools
             else
                 history.Add(plusCode10, DateTime.UtcNow.AddHours(hourDelay));
 
-            history = history.Where(h => h.Value > DateTime.UtcNow).ToDictionary(k => k.Key, v => v.Value);
+            if (nextPurge > DateTime.UtcNow) //This is a pretty heavy duty effort, so we shouldn't do it on every call.
+            {
+                history = history.Where(h => h.Value > DateTime.UtcNow).ToDictionary(k => k.Key, v => v.Value);
+                nextPurge = DateTime.UtcNow.AddMinutes(10);
+            }
             return grant;
         }
     }
