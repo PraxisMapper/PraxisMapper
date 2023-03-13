7b0bc8f64a8b78ba93337ef81823c96342925b37
PraxisMapper <praxismapper@gmail.com>
2023-03-13 18:24:25 -0400

    Docs continue

--- CoreComponents/Support/Singletons.cs
+++ CoreComponents/Support/Singletons.cs
@@ -7,6 +7,9 @@ using static PraxisCore.DbTables;
 namespace PraxisCore {
     public static class Singletons
     {
+        public static readonly NtsGeometryServices s = new NtsGeometryServices(PrecisionModel.Floating.Value, 4326);
+        public static readonly NetTopologySuite.IO.WKTReader geomTextReader = new NetTopologySuite.IO.WKTReader(s); // {DefaultSRID = 4326 };
+
         public static GeometryFactory geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(4326);
         public static PreparedGeometryFactory preparedGeometryFactory = new PreparedGeometryFactory();
         public static bool SimplifyAreas = false;
