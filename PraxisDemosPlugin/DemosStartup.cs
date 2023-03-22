1aa326006e40f7d232da9db704c7c145793c8f75
PraxisMapper <PraxisMapper@gmail.com>
2023-03-22 11:27:11 -0400

    progress on splatter demo

--- PraxisDemosPlugin/DemosStartup.cs
+++ PraxisDemosPlugin/DemosStartup.cs
@@ -1,10 +1,24 @@
 ﻿using PraxisCore.Support;
+using PraxisCore;
+using PraxisDemosPlugin.Controllers;
 
 namespace PraxisDemosPlugin
 {
     public class DemosStartup : IPraxisStartup
     {
-        public static void Startup() { 
+        public static void Startup() {
+            //foreach (var color in SplatterController.htmlColors)
+
+            TagParser.InsertStyles(DemoStyles.splatterStyle);
+
+            for (int color = 0; color < 32; color++)
+            {
+                var data = GenericData.GetGlobalData("splats-" + color).ToUTF8String();
+                if (!string.IsNullOrWhiteSpace(data))
+                    SplatterController.splatCollection.TryAdd(color, Singletons.geomTextReader.Read(data));
+                else
+                    SplatterController.splatCollection.TryAdd(color, Singletons.geometryFactory.CreatePolygon());
+            }
         }
     }
 }
