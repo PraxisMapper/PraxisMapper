9c423a67111b9f2e555caf159af8554df0ca04ce
PraxisMapper <praxismapper@gmail.com>
2023-08-17 22:20:53 -0400

    Add locks to Unroutine demo logic.

--- PraxisDemosPlugin/Controllers/UnRoutineController.cs
+++ PraxisDemosPlugin/Controllers/UnRoutineController.cs
@@ -51,46 +51,53 @@ namespace PraxisDemosPlugin.Controllers
         [Route("/[controller]/Enter/{plusCode10}")]
         public void Enter(string plusCode10)
         {
-            var places = Place.GetPlaces(plusCode10.ToGeoArea());
-            var placeTracker = GenericData.GetSecurePlayerData<List<VisitedPlace>>(accountId, "placesVisitedRB", password);
-            if (placeTracker == null)
-                placeTracker = new List<VisitedPlace>();
+            SimpleLockable.PerformWithLock(accountId, () =>
+            {
+                var places = Place.GetPlaces(plusCode10.ToGeoArea());
+                var placeTracker = GenericData.GetSecurePlayerData<List<VisitedPlace>>(accountId, "placesVisitedRB", password);
+                if (placeTracker == null)
+                    placeTracker = new List<VisitedPlace>();
 
-            foreach(var p in places)
-                if (!placeTracker.Any(pt => pt.placeId == p.PrivacyId))
-                    placeTracker.Add(new VisitedPlace() { placeId = p.PrivacyId, Name = TagParser.GetName(p), Category = p.StyleName });
+                foreach (var p in places)
+                    if (!placeTracker.Any(pt => pt.placeId == p.PrivacyId))
+                        placeTracker.Add(new VisitedPlace() { placeId = p.PrivacyId, Name = TagParser.GetName(p), Category = p.StyleName });
 
-            GenericData.SetSecurePlayerDataJson(accountId, "placesVisitedRB", placeTracker, password);
+                GenericData.SetSecurePlayerDataJson(accountId, "placesVisitedRB", placeTracker, password);
+            });
         }
 
-        public List<DbTables.Place> FindValidPlaces(string plusCode10, double minDistanceMeters, double maxDistanceMeters)
-        {
-            var places = Place.FindGoodTargetPlaces(plusCode10, minDistanceMeters, maxDistanceMeters, "suggestedGameplay");
+        //public List<DbTables.Place> FindValidPlaces(string plusCode10, double minDistanceMeters, double maxDistanceMeters)
+        //{
+        //    var places = Place.FindGoodTargetPlaces(plusCode10, minDistanceMeters, maxDistanceMeters, "suggestedGameplay");
 
-            var placeTracker = GenericData.GetSecurePlayerData<List<VisitedPlace>>(accountId, "placesVisitedRB", password);
-            if (placeTracker == null)
-                placeTracker = new List<VisitedPlace>();
-            var ignoreList = GenericData.GetSecurePlayerData<HashSet<Guid>>(accountId, "placesIgnoredRB", password);
-            if (ignoreList == null)
-                ignoreList = new HashSet<Guid>();
+        //    var placeTracker = GenericData.GetSecurePlayerData<List<VisitedPlace>>(accountId, "placesVisitedRB", password);
+        //    if (placeTracker == null)
+        //        placeTracker = new List<VisitedPlace>();
+        //    var ignoreList = GenericData.GetSecurePlayerData<HashSet<Guid>>(accountId, "placesIgnoredRB", password);
+        //    if (ignoreList == null)
+        //        ignoreList = new HashSet<Guid>();
 
-            //NOTE: some trails are unnamed in OSM, which could remove otherwise good choices. On the other hand, those unnamed
-            //trails could also just be sidewalks more often than not.
-            places = places.Where(p => !placeTracker.Any(pt => pt.placeId == p.PrivacyId) && !ignoreList.Contains(p.PrivacyId) && TagParser.GetName(p) != "").ToList();
+        //    //NOTE: some trails are unnamed in OSM, which could remove otherwise good choices. On the other hand, those unnamed
+        //    //trails could also just be sidewalks more often than not.
+        //    places = places.Where(p => !placeTracker.Any(pt => pt.placeId == p.PrivacyId) && !ignoreList.Contains(p.PrivacyId) && TagParser.GetName(p) != "").ToList();
 
-            return places;
-        }
+        //    return places;
+        //}
 
         [HttpPut]
         [Route("/[controller]/Ignore/{privacyId}")]
         public void IgnorePlace(string privacyId)
         {
-            Guid placeId = new Guid(privacyId);
-            var ignoreList = GenericData.GetSecurePlayerData<HashSet<Guid>>(accountId, "placesIgnoredRB", password);
-            if (ignoreList.Contains(placeId))
-                ignoreList.Add(placeId);
+            SimpleLockable.PerformWithLock(accountId, () =>
+            {
+                Guid placeId = new Guid(privacyId);
+                var ignoreList = GenericData.GetSecurePlayerData<HashSet<Guid>>(accountId, "placesIgnoredRB", password);
+                if (ignoreList.Contains(placeId))
+                    ignoreList.Add(placeId);
 
-            GenericData.SetSecurePlayerDataJson(accountId, "placesIgnoredRB", ignoreList, password);
+
+                GenericData.SetSecurePlayerDataJson(accountId, "placesIgnoredRB", ignoreList, password);
+            });
         }
 
         //GET /Target gives you the data on your target location
@@ -112,10 +119,11 @@ namespace PraxisDemosPlugin.Controllers
         [Route("/[controller]/Target/{plusCode10}")]
         public string PickTargetPlace(string plusCode10)
         {
+
             var settings = GenericData.GetPlayerData<PlayerSettings>(accountId, "rbSettings");
             if (settings == null)
                 settings = new PlayerSettings() { distancePref = 3, categories = "all" };
-            
+
             double minDistance = 1;
             double maxDistance = 2;
             switch (settings.distancePref)
@@ -142,7 +150,7 @@ namespace PraxisDemosPlugin.Controllers
             //NOTE: MariaDB specific SQL here.
             var place = db.Places.FromSql($"SELECT * FROM Places WHERE ST_Intersects(ElementGeometry, ST_GeomFromText({maxArea})) AND NOT ST_Intersects(ElementGeometry, ST_GeomFromText({minArea})) ORDER BY RAND() LIMIT 1").Include(p => p.Tags).Include(p => p.PlaceData).FirstOrDefault();
             //150ms total-ish vs 5000ms below.
-            
+
 
             //This way was about 5 seconds.
             //var places = FindValidPlaces(plusCode10, minDistance, maxDistance);
@@ -161,13 +169,16 @@ namespace PraxisDemosPlugin.Controllers
             }
 
 
+
+
             var distance = GeometrySupport.MetersDistanceTo(plusCode10.ToGeoArea().ToPoint(), place.ElementGeometry.Centroid);
-            return JsonSerializer.Serialize(new { 
-                Name = name, 
-                Category = place.StyleName, 
-                PlaceId = place.PrivacyId, 
-                Location = new OpenLocationCode(place.ElementGeometry.Centroid.Y, place.ElementGeometry.Centroid.X).Code, 
-                Distance = distance 
+            return JsonSerializer.Serialize(new
+            {
+                Name = name,
+                Category = place.StyleName,
+                PlaceId = place.PrivacyId,
+                Location = new OpenLocationCode(place.ElementGeometry.Centroid.Y, place.ElementGeometry.Centroid.X).Code,
+                Distance = distance
             });
 
         }
@@ -179,7 +190,7 @@ namespace PraxisDemosPlugin.Controllers
             var place = Place.GetPlace(placeId);
 
             //We will lock this preview image to a 900x900px box.
-            
+
             var imageArea = place.ElementGeometry.Envelope.ToGeoArea();
             if (place.ElementGeometry.GeometryType == "Point")
                 imageArea = place.ElementGeometry.Buffer(.000250).Envelope.ToGeoArea();
@@ -187,7 +198,7 @@ namespace PraxisDemosPlugin.Controllers
             var longerSide = imageArea.LatitudeHeight >= imageArea.LongitudeWidth ? imageArea.LatitudeHeight : imageArea.LongitudeWidth;
             //ImageStats istats = new ImageStats(imageArea, (int)(longerSide / ConstantValues.resolutionCell11Lon) * (int)MapTileSupport.GameTileScale, (int)(longerSide / ConstantValues.resolutionCell11Lat) * (int)MapTileSupport.GameTileScale);
             ImageStats istats = new ImageStats(imageArea, 900, 900);
-//            istats = MapTileSupport.ScaleBoundsCheck(istats, 900, 810000);
+            //            istats = MapTileSupport.ScaleBoundsCheck(istats, 900, 810000);
 
             var tile = MapTileSupport.MapTiles.DrawAreaAtSize(istats);
             return File(tile, "image/png");
@@ -216,17 +227,20 @@ namespace PraxisDemosPlugin.Controllers
         [Route("/[controller]/Options/{options}")]
         public void SetOptions(string options)
         {
-            var settings = GenericData.GetPlayerData<PlayerSettings>(accountId, "rbSettings");
-            if (settings == null)
-                settings = new PlayerSettings() { distancePref = 3, categories = "-park-university-natureReserve-cemetery-artsCulture-historical" };
+            SimpleLockable.PerformWithLock(accountId, () =>
+            {
+                var settings = GenericData.GetPlayerData<PlayerSettings>(accountId, "rbSettings");
+                if (settings == null)
+                    settings = new PlayerSettings() { distancePref = 3, categories = "-park-university-natureReserve-cemetery-artsCulture-historical" };
 
-            string[] newSettings = options.Split('~'); //sections are split by ~. 
-            settings.categories = newSettings[0]; //categories are split by | but the individual pieces aren't checked.
+                string[] newSettings = options.Split('~'); //sections are split by ~. 
+                settings.categories = newSettings[0]; //categories are split by | but the individual pieces aren't checked.
 
-            if (newSettings.Length > 1)
-                settings.distancePref = newSettings[1].ToInt();
+                if (newSettings.Length > 1)
+                    settings.distancePref = newSettings[1].ToInt();
 
-            GenericData.SetPlayerDataJson(accountId, "rbSettings", settings);
+                GenericData.SetPlayerDataJson(accountId, "rbSettings", settings);
+            });
         }
     }
 }
