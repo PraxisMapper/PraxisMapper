e993094d98ce6fdf141041837e4d3071cfe9a8f9
PraxisMapper <praxismapper@gmail.com>
2023-09-22 10:41:41 -0400

    GeometryTracker has now split. CellTracker is way faster at only tracking cells. GeometryTracker remains the item to store arbitrary shapes.

---
+++ CoreComponents/GameTools/CellTracker.cs
@@ -0,0 +1,52 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PraxisCore.GameTools {
+    /// <summary>
+    /// CellTracker allows you to save PlusCode Cell10 (or Cell11) values to be drawn to a geometry object later. This is useful for drawing a map of team-controlled territory, a player's complete
+    /// location history, or other things not tied to specific Places on the map.
+    /// If this is used for a player's location history, it MUST be saved with SetSecurePlayerData.
+    /// This is dramatically faster than GeometryTracker when only drawing PlusCode cells as squares. For arbitrary geometries, GeometryTracker is required.
+    /// </summary>
+    public sealed class CellTracker {
+
+
+        /// <summary>
+        
+        /// </summary>
+        public Dictionary<string, byte> Visited { get; set; } = new Dictionary<string, byte>();
+
+
+        /// <summary>
+        /// Add a PlusCode cell to the explored geometry. Can be any valid PlusCode size, though if you are tracking Cell11s consider using RecentPath instead of GeometryTracker.
+        /// </summary>
+        /// <param name="plusCode">A valid 10 or 11 digit PlusCode</param>
+        public void AddCell(string plusCode) 
+        {
+            Visited.TryAdd(plusCode, 1);
+        }
+
+        /// <summary>
+        /// Removes a PlusCode cell from the explored geometry. Can be any valid PlusCode size, though if you are tracking Cell11s consider using RecentPath instead of GeometryTracker.
+        /// </summary>
+        public void RemoveCell(string plusCode) 
+        {
+            Visited.Remove(plusCode);
+        }
+
+        public Geometry AsGeometry()
+        {
+            //Converts this to a geometry object for drawing purposes.
+            Geometry geo = Polygon.Empty;
+            List<Geometry> subItems = new List<Geometry>();
+
+            foreach(var entry in Visited.Keys)
+                subItems.Add(entry.ToPolygon());
+
+            geo = NetTopologySuite.Operation.Union.CascadedPolygonUnion.Union(subItems);
+            return geo;
+        }
+    }
+}
--- CoreComponents/GameTools/GeometryTracker.cs
+++ CoreComponents/GameTools/GeometryTracker.cs
@@ -1,10 +1,11 @@
 ﻿using NetTopologySuite.Geometries;
+using System;
 using System.Text.Json.Serialization;
 
 namespace PraxisCore.GameTools {
     /// <summary>
-    /// GeometryTracker allows you to save PlusCode Cells to an accumulating Geometry object. This is useful for drawing a map of team-controlled territory, a player's complete
-    /// location history, or other things not tied to specific Places on the map.
+    /// GeometryTracker allows you to save arbitrary Geometry shapes to an accumulating Geometry object. This is useful for drawing a map of team-controlled territory, places visited,
+    /// or other random shapes. If only tracking PlusCode cells, use CellTracker for dramatically better performance.
     /// If this is used for a player's location history, it MUST be saved with SetSecurePlayerData.
     /// </summary>
     public sealed class GeometryTracker {
@@ -15,48 +16,10 @@ namespace PraxisCore.GameTools {
         [JsonIgnore]
         public Geometry explored { get; set; } = Singletons.geometryFactory.CreatePolygon(); //This is the object most of the work will be done against
 
-        string _exploredAsText = "";
         /// <summary>
         /// The Well-Known Text (WKT) form of the explored geometry. This is persisted to the database and regenerated to the explored value when necessary.
         /// </summary>
         public string exploredAsText { get { return explored.ToText(); } set { explored = Singletons.geomTextReader.Read(value); } } //This is what gets saves as JSON to our database for simplicity, even if it incurs some processing overhead.
-        /// <summary>
-        /// Internal variable, used to skip populating the Geometry object if it's already been filled in.
-        /// </summary>
-        [JsonIgnore]
-        bool isPopulated = false;
-
-        /// <summary>
-        /// Fills in explored with the data from exploredAsText, if it has not yet been populated since loading from the database.
-        /// </summary>
-        //public void PopulateExplored() 
-        //{
-        //    if (!isPopulated) 
-        //    {
-        //        explored = GeometrySupport.GeometryFromWKT(exploredAsText);
-        //        isPopulated = true;
-        //    } 
-        //}
-
-        /// <summary>
-        /// Add a PlusCode cell to the explored geometry. Can be any valid PlusCode size, though if you are tracking Cell11s consider using RecentPath instead of GeometryTracker.
-        /// </summary>
-        /// <param name="plusCode">A valid PlusCode (without the +, if its 10 digits or longer)</param>
-        public void AddCell(string plusCode) 
-        {
-            //PopulateExplored();
-            //Lines that touch remain multipolygons. Unioning buffered areas leaves all their points in place. Simplify removes most redundant points.
-            explored = explored.Union(GeometrySupport.MakeBufferedGeoArea(plusCode.ToGeoArea(), 0.00000001).ToPolygon());
-        }
-
-        /// <summary>
-        /// Removes a PlusCode cell from the explored geometry. Can be any valid PlusCode size, though if you are tracking Cell11s consider using RecentPath instead of GeometryTracker.
-        /// </summary>
-        public void RemoveCell(string plusCode) 
-        {
-            //PopulateExplored();
-            explored = explored.Difference(GeometrySupport.MakeBufferedGeoArea(plusCode.ToGeoArea(), 0.00000001).ToPolygon()).Simplify(0.00000001);
-        }
 
         /// <summary>
         /// Add the given geometry object's area to the tracker's geometry.
@@ -64,7 +27,6 @@ namespace PraxisCore.GameTools {
         /// <param name="geo"></param>
         public void AddGeometry(Geometry geo)
         {
-            //PopulateExplored();
             explored = explored.Union(geo).Simplify(0.00000001);
         }
 
@@ -73,7 +35,6 @@ namespace PraxisCore.GameTools {
         /// </summary>
         public void RemoveGeometry(Geometry geo)
         {
-            //PopulateExplored();
             explored = explored.Difference(geo).Simplify(0.00000001);
         }
 
