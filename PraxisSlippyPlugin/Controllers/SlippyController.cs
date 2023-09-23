a8fb3859a744319765a6023ac492f8cbe061972a
PraxisMapper <praxismapper@gmail.com>
2023-09-22 22:11:47 -0400

    Slippy can now load values from the database, allows other plugins to give it new overlays/base values.

--- PraxisSlippyPlugin/Controllers/SlippyController.cs
+++ PraxisSlippyPlugin/Controllers/SlippyController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PraxisCore;
 using PraxisCore.Support;
+using System.Text.Json;
 
 namespace PraxisMapper.Controllers {
     [Route("[controller]")]
@@ -14,6 +16,21 @@ namespace PraxisMapper.Controllers {
             catch (Exception ex) {
                 return null;
             }
+        }
+
+        [HttpGet]
+        [Route("/[controller]/Configs")]
+        public JsonResult Configs()
+        {
+            //TODO: determine how to pick which entries are Overlays and which are the main BG tiles.
+            //Overlays have a transparent background, base layers have an opaque background.
+            //String of values. 
+            var db = new PraxisContext();
+            var baseLayers = db.GlobalData.Where(g => g.DataKey.StartsWith("SlippyBase-")).ToList();
+            var overlays = db.GlobalData.Where(g => g.DataKey.StartsWith("SlippyOverlay-")).ToList();
+            var results = baseLayers.Select(c => new { key = c.DataKey.Replace("SlippyBase-", ""), value = c.DataValue.ToUTF8String(), isOverlay = false }).ToList();
+            results.AddRange(overlays.Select(c => new { key = c.DataKey.Replace("SlippyOverlay-", ""), value = c.DataValue.ToUTF8String(), isOverlay = true }).ToList());
+            return Json(results);
         }
     }
 }
