20641d05a8c5e7c9c289ff90b2e5be8ef5d884e2
PraxisMapper <praxismapper@gmail.com>
2023-11-19 15:45:34 -0500

    Add all filled adminBounds overlays to default Slippy list

--- PraxisSlippyPlugin/SlippyStartup.cs
+++ PraxisSlippyPlugin/SlippyStartup.cs
@@ -16,9 +16,18 @@ public class PraxisSlippyStartup : IPraxisStartup
         GenericData.SetGlobalData("SlippyOverlay-All Admin Bounds", "MapTile/Slippy/adminBounds");
         GenericData.SetGlobalData("SlippyOverlay-All Place Outlines", "MapTile/Slippy/outlines");
         GenericData.SetGlobalData("SlippyOverlay-Area Control", "MapTile/Slippy/teamColor");
-        GenericData.SetGlobalData("SlippyOverlay-Cities (filled)", "MapTile/Slippy/adminBoundsFilled/city");
-        GenericData.SetGlobalData("SlippyOverlay-States (filled)", "MapTile/Slippy/adminBoundsFilled/state");
         GenericData.SetGlobalData("SlippyOverlay-Countries (filled)", "MapTile/Slippy/adminBoundsFilled/country");
+        GenericData.SetGlobalData("SlippyOverlay-Regions(filled)", "MapTile/Slippy/adminBoundsFilled/region");
+        GenericData.SetGlobalData("SlippyOverlay-States (filled)", "MapTile/Slippy/adminBoundsFilled/state");
+        GenericData.SetGlobalData("SlippyOverlay-Admin Level 5 (filled)", "MapTile/Slippy/adminBoundsFilled/admin5");
+        GenericData.SetGlobalData("SlippyOverlay-Counties (filled)", "MapTile/Slippy/adminBoundsFilled/county");
+        GenericData.SetGlobalData("SlippyOverlay-Townships (filled)", "MapTile/Slippy/adminBoundsFilled/township");
+        GenericData.SetGlobalData("SlippyOverlay-Cities (filled)", "MapTile/Slippy/adminBoundsFilled/city");
+        GenericData.SetGlobalData("SlippyOverlay-Wards (filled)", "MapTile/Slippy/adminBoundsFilled/ward");
+        GenericData.SetGlobalData("SlippyOverlay-Neighborhoods (filled)", "MapTile/Slippy/adminBoundsFilled/neighborhood");
+        GenericData.SetGlobalData("SlippyOverlay-Admin Level 11 (filled)", "MapTile/Slippy/adminBoundsFilled/admin11");
+
+
 
         Log.WriteLog("[SlippyPlugin]: Loaded default values to database", Log.VerbosityLevels.High);
     }
