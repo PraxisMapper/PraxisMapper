712138c3b05863ee1951d15e7c785cd994db5b1c
PraxisMapper <praxismapper@gmail.com>
2024-01-16 01:30:17 -0500

    Offline data v2 format and logic coming along. Multipolygons and polygons with holes stll in progress.

--- PraxisOfflineDataPlugin/Controllers/OfflineController.cs
+++ PraxisOfflineDataPlugin/Controllers/OfflineController.cs
@@ -7,16 +7,21 @@ using PraxisCore.Support;
 using PraxisMapper.Classes;
 using System.Collections.Concurrent;
 using System.Text.Json;
+using System.Xml.Schema;
+using static Azure.Core.HttpHeader;
 
-namespace PraxisOfflineDataPlugin.Controllers {
+namespace PraxisOfflineDataPlugin.Controllers
+{
     [Route("[controller]")]
-    public class OfflineController : Controller, IPraxisPlugin {
+    public class OfflineController : Controller, IPraxisPlugin
+    {
         //NOTE: for more accurate data, I could save cell10 info in the final dictionary.
         //THis would be be better saved for a Cell6 or smaller area, but that could be generated on demand once,
         //then saved and sent on every other request.
         [HttpGet]
         [Route("/[controller]/Small/{plusCode6}")]
-        public string GetSmallTerrainData(string plusCode6) {
+        public string GetSmallTerrainData(string plusCode6)
+        {
             Response.Headers.Add("X-noPerfTrack", "Offline/Small/VARSREMOVED");
             GeoArea box6 = plusCode6.ToGeoArea();
             var quickplaces = Place.GetPlaces(box6);
@@ -44,7 +49,8 @@ namespace PraxisOfflineDataPlugin.Controllers {
             terrainDict["index"] = new ConcurrentDictionary<string, ConcurrentDictionary<string, ConcurrentDictionary<string, string>>>();
             terrainDict["index"][String.Join("|", index.Select(i => i.Key + "," + i.Value))] = new ConcurrentDictionary<string, ConcurrentDictionary<string, string>>();
 
-            Parallel.ForEach(GetCellCombos(), (cell8) => {
+            Parallel.ForEach(GetCellCombos(), (cell8) =>
+            {
                 string pluscode = plusCode6 + cell8;
                 GeoArea box = pluscode.ToGeoArea();
                 var places = Place.GetPlaces(box, quickplaces);
@@ -57,7 +63,8 @@ namespace PraxisOfflineDataPlugin.Controllers {
                 var terrainInfo = AreaStyle.GetAreaDetails(ref box, ref places);
                 var terrainsPresent = terrainInfo.Select(t => t.data.style).Distinct().ToList();
 
-                if (terrainsPresent.Count > 0) {
+                if (terrainsPresent.Count > 0)
+                {
                     string concatTerrain = String.Join("|", terrainsPresent.Select(t => index[t]));
                     terrainDict[cell2][cell4][cell6][cell8] = concatTerrain;
                 }
@@ -68,7 +75,8 @@ namespace PraxisOfflineDataPlugin.Controllers {
 
         [HttpGet]
         [Route("/[controller]/All")]
-        public string GetAllOfflineData() {
+        public string GetAllOfflineData()
+        {
             //NOTE: this is not intended to be called by users, since this reads the entire database. 
             //This is for an admin to use to create data for an application to reference.
             PraxisAuthentication.GetAuthInfo(Response, out var accountId, out var password);
@@ -84,16 +92,19 @@ namespace PraxisOfflineDataPlugin.Controllers {
             terrainDict["index"] = new ConcurrentDictionary<string, ConcurrentDictionary<string, ConcurrentDictionary<string, string>>>();
             terrainDict["index"][String.Join("|", index.Select(i => i.Key + "," + i.Value))] = new ConcurrentDictionary<string, ConcurrentDictionary<string, string>>();
 
-            foreach (var cell2 in GetCell2Combos()) {
+            foreach (var cell2 in GetCell2Combos())
+            {
                 var place2 = cell2.ToPolygon();
                 var placeTest = db.Places.Any(p => p.ElementGeometry.Intersects(place2));
                 if (!placeTest)
                     continue;
 
                 terrainDict[cell2] = new ConcurrentDictionary<string, ConcurrentDictionary<string, ConcurrentDictionary<string, string>>>();
-                foreach (var cell4 in GetCellCombos()) {
+                foreach (var cell4 in GetCellCombos())
+                {
                     terrainDict[cell2][cell4] = new ConcurrentDictionary<string, ConcurrentDictionary<string, string>>();
-                    foreach (var cell6 in GetCellCombos()) {
+                    foreach (var cell6 in GetCellCombos())
+                    {
                         string pluscode6 = cell2 + cell4 + cell6;
                         GeoArea box6 = pluscode6.ToGeoArea();
                         var quickplaces = Place.GetPlaces(box6);
@@ -106,7 +117,8 @@ namespace PraxisOfflineDataPlugin.Controllers {
                             if (place.ElementGeometry.Coordinates.Length > 1000)
                                 place.ElementGeometry = place.ElementGeometry.Intersection(box6.ToPolygon());
 
-                        Parallel.ForEach(GetCellCombos(), (cell8) => {
+                        Parallel.ForEach(GetCellCombos(), (cell8) =>
+                        {
                             string pluscode = pluscode6 + cell8;
                             GeoArea box = pluscode.ToGeoArea();
                             var places = Place.GetPlaces(box, quickplaces);
@@ -119,7 +131,8 @@ namespace PraxisOfflineDataPlugin.Controllers {
                             var terrainInfo = AreaStyle.GetAreaDetails(ref box, ref places);
                             var terrainsPresent = terrainInfo.Select(t => t.data.style).Distinct().ToList();
 
-                            if (terrainsPresent.Count > 0) {
+                            if (terrainsPresent.Count > 0)
+                            {
                                 string concatTerrain = String.Join("|", terrainsPresent.Select(t => index[t])); //indexed ID of each type.
                                 terrainDict[cell2][cell4][cell6][cell8] = concatTerrain;
                             }
@@ -142,28 +155,34 @@ namespace PraxisOfflineDataPlugin.Controllers {
         private static Dictionary<string, int> GetTerrainIndex(string style = "mapTiles")
         {
             var dict = new Dictionary<string, int>();
-            foreach (var entry in TagParser.allStyleGroups[style]) {
-                if (entry.Value.IsGameElement) {
+            foreach (var entry in TagParser.allStyleGroups[style])
+            {
+                if (entry.Value.IsGameElement)
+                {
                     dict.Add(entry.Key, dict.Count + 1);
                 }
             }
             return dict;
         }
 
-        static List<string> GetCellCombos() {
+        static List<string> GetCellCombos()
+        {
             var list = new List<string>(400);
             foreach (var Yletter in OpenLocationCode.CodeAlphabet)
-                foreach (var Xletter in OpenLocationCode.CodeAlphabet) {
+                foreach (var Xletter in OpenLocationCode.CodeAlphabet)
+                {
                     list.Add(String.Concat(Yletter, Xletter));
                 }
 
             return list;
         }
 
-        static List<string> GetCell2Combos() {
+        static List<string> GetCell2Combos()
+        {
             var list = new List<string>(400);
             foreach (var Yletter in OpenLocationCode.CodeAlphabet.Take(9))
-                foreach (var Xletter in OpenLocationCode.CodeAlphabet.Take(18)) {
+                foreach (var Xletter in OpenLocationCode.CodeAlphabet.Take(18))
+                {
                     list.Add(String.Concat(Yletter, Xletter));
                 }
 
@@ -174,22 +193,23 @@ namespace PraxisOfflineDataPlugin.Controllers {
         public class OfflineDataV2
         {
             public List<OfflinePlaceEntry> entries { get; set; }
-            public Dictionary<string, int> nameTable { get; set; }
-            public int[][] nameIds { get; set; }
+            public Dictionary<int, string> nameTable { get; set; } //id, name
+            public Dictionary<string, int> gridNames { get; set; } //pluscode, nameTable entry id
         }
 
-        public class OfflinePlaceEntry {
-            public int nameTableId { get; set; }
-            public int terrainId { get; set; } //which style entry this place is
-            public int geoType { get; set; } //1 = point, 2 = line OR hollow shape, 3 = filled shape.
+        public class OfflinePlaceEntry
+        {
+            public int nid { get; set; } //nametable id
+            public int tid { get; set; } //terrain id, which style entry this place is
+            public int gt { get; set; } //geometry type. 1 = point, 2 = line OR hollow shape, 3 = filled shape.
             //public string geometry { get; set; }
-            public string Points { get; set; } //alt-take on geometry.
+            public string p { get; set; } //Points, local to the given PlusCode
         }
 
         [HttpGet]
-        [Route("/[controller]/V2/{plusCode8}")]
-        [Route("/[controller]/V2/{plusCode8}/{styleSet}")]
-        public string GetOfflineDataV2(string plusCode8, string styleSet = "mapTiles")
+        [Route("/[controller]/V2/{plusCode}")]
+        [Route("/[controller]/V2/{plusCode}/{styleSet}")]
+        public string GetOfflineDataV2(string plusCode, string styleSet = "mapTiles")
         {
             //Trying a second approach to this. I want a smaller set of data, but I also want to expand whats available in this.
             //This assumes that a server exists, but it lives primarily NOT to draw tiles itself, but to parse data down for a specific game client which does that work.
@@ -198,67 +218,104 @@ namespace PraxisOfflineDataPlugin.Controllers {
             db.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             db.ChangeTracker.AutoDetectChangesEnabled = false;
 
-            var addGeo = true;
-
             //save and load this from the db if we've generated it before.
-            var existingData = db.AreaData.FirstOrDefault(a => a.PlusCode == plusCode8 && a.DataKey == "offlineV2");
+            var existingData = db.AreaData.FirstOrDefault(a => a.PlusCode == plusCode && a.DataKey == "offlineV2");
             if (existingData != null)
                 return existingData.DataValue.ToUTF8String();
 
-            var cell8 = plusCode8.ToGeoArea();
+            var cell8 = plusCode.ToGeoArea();
             var cell8Poly = cell8.ToPolygon();
             var placeData = Place.GetPlaces(cell8, styleSet: styleSet, dataKey: styleSet);
-            int nameCounter = 0; //used to determine nametable key
+            int nameCounter = 1; //used to determine nametable key
 
             List<OfflinePlaceEntry> entries = new List<OfflinePlaceEntry>(placeData.Count);
             Dictionary<string, int> nametable = new Dictionary<string, int>(); //name, id
 
-            foreach(var place in placeData)
+            var min = cell8.Min;
+            foreach (var place in placeData)
             {
-                var offline = new OfflinePlaceEntry();
-                offline.terrainId = (int)TagParser.allStyleGroups[styleSet][place.StyleName].Id; //Client will need to know what this ID means.
-                //Crop this geometry to the Cell8 we're looking at to minimize data sent over, and simplify it to a Cell10 resolution.
-                if (addGeo)
-                {
-                    //TODO: this may need to convert a polygon to a line before calling Intersection if the style only has Stroke items.
-                    //TODO: convert points to 0,100 pixel-space coordinates. Subtract min-coord value for the plus code, then value / Cell1res?
-
-                    var min = cell8.Min;
-                    place.ElementGeometry = place.ElementGeometry.Intersection(cell8Poly).Simplify(ConstantValues.resolutionCell10);
-                    //offline.geometry = place.ElementGeometry.AsText();
-                    offline.geoType = place.ElementGeometry.GeometryType == "Point" ? 1 : place.ElementGeometry.GeometryType == "LineString" ? 2 : 3;
-
-                    //NOTE: if I'm locking these geometry items to a tile, I could convert these points in the geometry to integers between 0 and 100, effectively
-                    //letting me draw Cell11 pixel-precise points from this info for an 80x100 base-scale image, and would be shorter stringified for JSON vs floats/doubles.
-                    offline.Points = string.Join("|", place.ElementGeometry.Coordinates.Select(c => (int)((c.X - min.Longitude) / ConstantValues.resolutionCell11Lon) + "," + ((int)(c.Y - min.Latitude) / ConstantValues.resolutionCell11Lat)));
-                }
-
-                //TODO: AreaStyle.GetAreaDetails() style crawl over all listed places to determine names and values?
+                place.ElementGeometry = place.ElementGeometry.Intersection(cell8Poly).Simplify(ConstantValues.resolutionCell11Lon);
 
                 var name = TagParser.GetName(place);
+                int nameID = 0;
                 if (!string.IsNullOrWhiteSpace(name))
                 {
-                    if (!nametable.TryGetValue(name, out var nameid))
+                    if (!nametable.TryGetValue(name, out var nameval))
                     {
-                        nameid = nameCounter;
-                        nametable.Add(name, nameid);
+                        nameval = nameCounter;
+                        nametable.Add(name, nameval);
                         nameCounter++;
                         //attach to this item.
                     }
-                    offline.nameTableId = nameid;
+                    nameID = nameval;
                 }
-                entries.Add(offline);
+
+                var style = TagParser.allStyleGroups[styleSet][place.StyleName];
+                var coordSets = GetCoordEntries(place, cell8.Min);
+                foreach (var coordSet in coordSets)
+                {
+                    //This is where most of the stuff below should actually be handled.
+                    var offline = new OfflinePlaceEntry();
+                    offline.nid = nameID;
+                    offline.tid = (int)style.Id; //Client will need to know what this ID means.
+                    
+                    //This should be the same for all items in the place. Multipolygons will either be 
+                    offline.gt = place.ElementGeometry.GeometryType == "Point" ? 1 : place.ElementGeometry.GeometryType == "LineString" ? 2 : style.PaintOperations.All(p => p.FillOrStroke == "stroke") ? 2 : 3;
+
+                    //NOTE: if I'm locking these geometry items to a tile, I could convert these points in the geometry to integers between 0 and 100, effectively
+                    //letting me draw Cell11 pixel-precise points from this info for an 80x100 base-scale image, and would be shorter stringified for JSON vs floats/doubles.
+                    offline.p = string.Join("|", place.ElementGeometry.Coordinates.Select(c => (int)((c.X - min.Longitude) / ConstantValues.resolutionCell11Lon) + "," + ((int)((c.Y - min.Latitude) / ConstantValues.resolutionCell11Lat))));
+
+                    entries.Add(offline);
+                }
             }
 
+            //TODO: this is not yet totally optimized, but it's sufficient fast as-is for use.
+            var cell82 = (GeoArea)cell8;
+            var terrainInfo = AreaStyle.GetAreaDetails(ref cell82, ref placeData);
+            Dictionary<string, int> nameInfo = terrainInfo.Where(t => t.data.name != "").Select(t => new { t.plusCode, nameId = nametable[t.data.name] }).ToDictionary(k => k.plusCode.Replace(plusCode, ""), v => v.nameId);
+
             var finalData = new OfflineDataV2();
-            finalData.nameTable = nametable;
+            finalData.nameTable = nametable.ToDictionary(k => k.Value, v => v.Key);
             finalData.entries = entries;
+            finalData.gridNames = nameInfo;
 
             string data = JsonSerializer.Serialize(finalData);
-            db.AreaData.Add(new DbTables.AreaData() { PlusCode = plusCode8, DataKey = "offlineV2", DataValue = data.ToByteArrayUTF8(), Expiration = DateTime.UtcNow.AddYears(10) });
-            db.SaveChanges();
-
+            GenericData.SetAreaData(plusCode, "offlineV2", data, (10 * 365 * 24 * 60 * 1000.0)); //10 years in ms?
             return data;
+        }
+
+        public List<string> GetCoordEntries(DbTables.Place place, GeoPoint min)
+        {
+            List<string> points = new List<string>();
+
+            if (place.ElementGeometry.GeometryType == "MultiPolygon")
+            {
+                foreach (var poly in ((MultiPolygon)place.ElementGeometry).Geometries) //This should be the same as the Polygon code below.
+                {
+                    points.Add(string.Join("|", poly.Coordinates.Select(c => (int)((c.X - min.Longitude) / ConstantValues.resolutionCell11Lon) + "," + ((int)((c.Y - min.Latitude) / ConstantValues.resolutionCell11Lat)))));
+                }
+            }
+            else if (place.ElementGeometry.GeometryType == "Polygon")
+            {
+                var poly = place.ElementGeometry as Polygon;
+                if (poly.Holes.Length == 0)
+                    points.Add(string.Join("|", poly.Coordinates.Select(c => (int)((c.X - min.Longitude) / ConstantValues.resolutionCell11Lon) + "," + ((int)((c.Y - min.Latitude) / ConstantValues.resolutionCell11Lat)))));
+                else
+                {
+                    //How to handle ones with holes?
+                    //split the outer ring on the center of the hole? Draw a straight line through all holes and split on that?
+
+                }
+            }
+            else
+                points.Add(string.Join("|", place.ElementGeometry.Coordinates.Select(c => (int)((c.X - min.Longitude) / ConstantValues.resolutionCell11Lon) + "," + ((int)((c.Y - min.Latitude) / ConstantValues.resolutionCell11Lat)))));
+
+
+            //NOTE: if I'm locking these geometry items to a tile, I could convert these points in the geometry to integers between 0 and 100, effectively
+            //letting me draw Cell11 pixel-precise points from this info for an 80x100 base-scale image, and would be shorter stringified for JSON vs floats/doubles.
+
+            return points;
         }
     }
 }
