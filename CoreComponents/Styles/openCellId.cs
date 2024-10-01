983647da0556cd73214ab1502afd6f22360021e5
PraxisMapper <praxismapper@gmail.com>
2024-10-01 00:05:29 -0400

    Initial support for OpenCellId data.

---
+++ CoreComponents/Styles/openCellId.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PraxisCore.DbTables;
+
+namespace PraxisCore.Styles
+{
+    public static class openCellId 
+        //Cell tower area estimates. Note that these 'cells' are per antenna, not per support tower, and are located. in the middle
+        //of the coverage area.
+    {
+        public static List<StyleEntry> style = new List<StyleEntry>()
+        {
+            new StyleEntry() { MatchOrder = 1, Name ="cell",  StyleSet = "openCellId",
+                PaintOperations = new List<StylePaint>() { //Making linewidthdegrees = 0 means it will always draw as 1 px wide, at least with SkiaSharp
+                    new StylePaint() { HtmlColorCode = "000000", FillOrStroke = "stroke", LineWidthDegrees=0, LinePattern= "solid", LayerId = 100 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key = "*", Value = "*", MatchType = "tagged"},
+            }},
+            //background is a mandatory style entry name, but its transparent here.
+            new StyleEntry() { MatchOrder = 10000, Name ="background",  StyleSet = "openCellId",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "00000000", FillOrStroke = "fill", LineWidthDegrees=0.00000625F, LinePattern= "solid", LayerId = 101 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() {Key = "bg", Value = "bg", MatchType = "equals"},
+            }},
+            //this name needs to exist because of the default style using this name.
+            new StyleEntry() { MatchOrder = 10001, Name ="unmatched",  StyleSet = "openCellId",
+                PaintOperations = new List<StylePaint>() {
+                    new StylePaint() { HtmlColorCode = "00000000", FillOrStroke = "fill", LineWidthDegrees=0.00000625F, LinePattern= "solid", LayerId = 100 }
+                },
+                StyleMatchRules = new List<StyleMatchRule>() {
+                    new StyleMatchRule() { Key = "a", Value = "s", MatchType = "equals" }}
+            },
+        };
+    }
+}
