ffff3b9a4802ef24d3581c993bcba961eb88aa0a
PraxisMapper <praxismapper@gmail.com>
2024-12-06 13:35:29 -0500

    Part of previous commit.

--- CoreComponents/Support/IPraxisPlugin.cs
+++ CoreComponents/Support/IPraxisPlugin.cs
@@ -7,6 +7,7 @@ namespace PraxisCore.Support
         //NOTE: additions here need to be made to Chat, Demos, Muni, Offline?, Slippy, and Version plugins, plus actual games.
         //public string PrivacyPolicy; //
         //public List<DbTables.StyleEntry> Styles; //could automatically import styles?
+        public static string PrivacyPolicy => "";
     }
 
     public interface IPraxisStartup
