e8d7e11f8c6ea76ad125641929ddd3b79c40acf3
PraxisMapper <praxismapper@gmail.com>
2024-12-06 13:33:36 -0500

    Server controller now auto-swagger doc'd.

--- PraxisMapper/Controllers/ServerController.cs
+++ PraxisMapper/Controllers/ServerController.cs
@@ -1,19 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Identity.Client;
 using PraxisCore;
 using PraxisMapper.Classes;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Text.Json;
 using static PraxisCore.DbTables;
 
 namespace PraxisMapper.Controllers
@@ -33,6 +30,11 @@ namespace PraxisMapper.Controllers
             cache = memoryCacheSingleton;
         }
 
+        /// <summary>
+        /// Returns "OK" to prove the connection works
+        /// </summary>
+        /// <returns>The string "OK"</returns>
+        /// <remarks>This is intended to let the dev/admin confirm the server is up and configured as expected.</remarks>
         [HttpGet]
         [Route("/[controller]/Test")]
         public string Test()
@@ -41,6 +43,12 @@ namespace PraxisMapper.Controllers
             return "OK";
         }
 
+        /// <summary>
+        /// Find the geometric area the server supports
+        /// </summary>
+        /// <returns>A string in "minLat|minLon|maxLat|maxLon" format, indicating the bounding box where the server will process requests.</returns>
+        /// <remarks>The server, by default, checks bounds on most area-based requests and rejects any that are outside of this box. 
+        /// It's set during initial bootstrapping after data is loaded, and can be adjusted manually by editing the values in the ServerSettings database table.</remarks>
         [HttpGet]
         [Route("/[controller]/ServerBounds")]
         public string GetServerBounds()
@@ -49,18 +57,20 @@ namespace PraxisMapper.Controllers
             return bounds.SouthBound + "|" + bounds.WestBound + "|" + bounds.NorthBound + "|" + bounds.EastBound;
         }
 
+        /// <summary>
+        /// Deletes the requesting user's account.
+        /// </summary>
+        /// <returns>the number of rows deleted from the database</returns>
+        /// <remarks>Initially implemented as GDPR compliance, made available to all users. 
+        /// Expected to be called from the GDPR page, but could be exposed in a client.
+        /// Determines user by checking headers, as logged in actions will send an AuthKey connected to the username and password.</remarks>
         [HttpDelete]
         [Route("/[controller]/Account/")]
         public int DeleteUser()
         {
             //GDPR compliance requires this to exist and be available to the user. 
             //Custom games that attach players to locations may need additional logic to fully meet legal requirements.
-            //These 2 lines require PraxisAuth enabled, which you should have on anyways if you're using accounts.
-
-            //TODO: use the getauthinfo call here.
-            var accountId = Response.Headers["X-account"].ToString();
-            var password = Response.Headers["X-internalPwd"].ToString();
-
+            PraxisAuthentication.GetAuthInfo(Response, out var accountId, out var password);
 
             using var db = new PraxisContext();
             db.ChangeTracker.AutoDetectChangesEnabled = false;
@@ -72,6 +82,11 @@ namespace PraxisMapper.Controllers
             return db.SaveChanges();
         }
 
+        /// <summary>
+        /// Incomplete, Do not use
+        /// </summary>
+        /// <returns>a bool indicating if anti-cheat is enabled</returns>
+        /// <remarks>The current AntiCheat logic is horribly incomplete, and cannot be recommended. Have the server do all the work if you want data to be verifiable.</remarks>
         [HttpGet]
         [Route("/[controller]/UseAntiCheat")]
         public bool UseAntiCheat()
@@ -79,6 +94,11 @@ namespace PraxisMapper.Controllers
             return Configuration.GetValue<bool>("enableAntiCheat");
         }
 
+        /// <summary>
+        /// Get a Message Of The Day 
+        /// </summary>
+        /// <returns>A string to use as the Message of the Day</returns>
+        /// <remarks>Some games may have a quick bit of info from the developer pop up somewhere that changes frequently, this is intended to enable that.</remarks>
         [HttpGet]
         [Route("/[controller]/MOTD")]
         [Route("/[controller]/Message")]
@@ -96,6 +116,12 @@ namespace PraxisMapper.Controllers
             return message;
         }
 
+        /// <summary>
+        /// Incomplete, do not use.
+        /// </summary>
+        /// <param name="filename">a filename</param>
+        /// <remarks>This is a very early attempt an anti-cheat, where the client uploads a file and the server validates its the same, correct file.
+        /// This isn't terribly solid anti-cheat, as a malicious client simply needs to upload the original code file and use its own modified version instead.</remarks>
         [HttpPut]
         [Route("/[controller]/AntiCheat/{filename}")]
         public void AntiCheat(string filename)
@@ -121,6 +147,13 @@ namespace PraxisMapper.Controllers
             }
         }
 
+        /// <summary>
+        /// Log in to the server
+        /// </summary>
+        /// <param name="accountId">DEPRECATED - The user's account id</param>
+        /// <param name="password">DEPRECATED - The password for the account</param>
+        /// <returns>an AuthDataResponse if the username and password are valid and match, or nothing if they don't match, don't exist, or the user is banned.  </returns>
+        /// <remarks>Put the username/password in the JSON body posted here, not in the query string parameters. That process works, but isnt't secure without SSL (and maybe even then)</remarks>
         [HttpGet]
         [HttpPut]
         [Route("/[controller]/Login")]
@@ -158,6 +191,10 @@ namespace PraxisMapper.Controllers
             return null;
         }
 
+        /// <summary>
+        /// Log a user out.
+        /// </summary>
+        /// <remarks>Removes the user's AuthKey from memory </remarks>
         [HttpGet]
         [HttpPut]
         [Route("/[controller]/Logout")]
@@ -168,6 +205,14 @@ namespace PraxisMapper.Controllers
             PraxisAuthentication.RemoveEntry(key);
         }
 
+        /// <summary>
+        /// Create a new account
+        /// </summary>
+        /// <param name="accountId">DEPRECATED - The user's account id</param>
+        /// <param name="password">DEPRECATED - The password for the account</param>
+        /// <returns>true if the account was created, and false if not.</returns>
+        /// <remarks>Put the username/password in the JSON body posted here, not in the query string parameters. That process works, but isnt't secure without SSL (and maybe even then).
+        /// This call IS SUPPOSED TO be slow. PraxisMapper uses BCrypt to secure account info, and the rough target time for solid security is about 250ms to create a password hash.</remarks>
         [HttpPut]
         [Route("/[controller]/CreateAccount")]
         [Route("/[controller]/CreateAccount/{accountId}/{password}")]
@@ -198,6 +243,17 @@ namespace PraxisMapper.Controllers
             return GenericData.EncryptPassword(accountId, password, Configuration.GetValue<int>("PasswordRounds"));
         }
 
+        /// <summary>
+        /// Change the password on an account.
+        /// </summary>
+        /// <param name="accountId">DEPRECATED - The user's account id</param>
+        /// <param name="passwordOld">DEPRECATED - The user's current password</param>
+        /// <param name="passwordNew">DEPRECATED - The user's future/new password</param>
+        /// <returns>true if the password was changed, or false if it wasnt</returns>
+        /// <remarks>Put the username/passwords in the JSON body posted here, not in the query string parameters. That process works, but isnt't secure without SSL (and maybe even then).
+        /// This call IS SUPPOSED TO be slow. PraxisMapper uses BCrypt to secure account info, and the rough target time for solid security is about 250ms to create a password hash.
+        /// This will be slower than creating an account, because this must update ALL secure data rows in the database for this account.
+        /// </remarks>
         [HttpPut]
         [Route("/[controller]/ChangePassword")]
         [Route("/[controller]/ChangePassword/{accountId}/{passwordOld}/{passwordNew}")]
@@ -244,6 +300,11 @@ namespace PraxisMapper.Controllers
             return false;
         }
 
+        /// <summary>
+        /// get a random point anywhere within the server boundaries
+        /// </summary>
+        /// <returns>a random point anywhere within the server boundaries in "lat|lon" format</returns>
+        /// <remarks> This does't save anyting to the server, so it's specific to whichever client requested this and may not be useful if the bounds are larger than a single city.</remarks>
         [HttpGet]
         [Route("/[controller]/RandomPoint")]
         public string RandomPoint()
@@ -252,6 +313,14 @@ namespace PraxisMapper.Controllers
             return PraxisCore.Place.RandomPoint(bounds);
         }
 
+        /// <summary>
+        /// Get random numbers based on a specific area
+        /// </summary>
+        /// <param name="plusCode">the PlusCode to get values for</param>
+        /// <param name="count">how many values to return</param>
+        /// <returns>An array of integers</returns>
+        /// <remarks>This was added as a way for the server to pass clients the same random numbers for the same area.
+        /// This is probably better implemented on the client, but this remains available here.</remarks>
         [HttpGet]
         [Route("/[controller]/RandomValues/{plusCode}/{count}")]
         public List<int> GetRandomValuesForArea(string plusCode, int count)
@@ -264,6 +333,19 @@ namespace PraxisMapper.Controllers
             return values;
         }
 
+        /// <summary>
+        /// Export all saved data on the user, per GDPR law
+        /// </summary>
+        /// <param name="username">DEPRECATED - the account id to pull data from</param>
+        /// <param name="pwd">DEPRECATED - the password for the account in question</param>
+        /// <returns>The internal data on the account, plus all data keys stored for the account</returns>
+        /// <remarks>Put the username/password in the JSON body posted here, not in the query string parameters. That process works, but isnt't secure without SSL (and maybe even then).
+        /// GDPR compliance requires users to be able to request all the data on them. This allows for this without manual admin effort.
+        /// Since this is the user requesting their own data, encrypted data keys are DECRYPTED and returned to the user.
+        /// The first line is the account data, in "key1:value1|key2:value2" format for the accountID, bannedUntil, bannedReason, and isAdmin columns.
+        /// The remaining lines will be entries from the PlayerData table for the accountId, in "key:value/n" format.
+        /// Lines that were encrypted in the database will have "[DECRYPTED]" after "key" in the response.
+        ///</remarks>
         [HttpGet]
         [Route("/[controller]/GdprExport")]
         [Route("/[controller]/GdprExport/{username}/{pwd}")]
@@ -312,6 +394,10 @@ namespace PraxisMapper.Controllers
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Opens the GDPR self-service page.
+        /// </summary>
+        /// <returns>The GDPR self-service view.</returns>
         [HttpGet]
         [Route("/[controller]/Gdpr")]
         public IActionResult GDPR()
@@ -319,6 +405,12 @@ namespace PraxisMapper.Controllers
             return View();
         }
 
+        /// <summary>
+        /// See the PraxisMapper Privacy Policy
+        /// </summary>
+        /// <returns>The whole of the Privacy Policy</returns>
+        /// <remarks>I initially added this because the Google Play Store requires a Privacy Policy from a live webserver.
+        /// This will include the hard-coded PraxisMapper value, and then adds on any privacy policy addendums found in any of the plugins.</remarks>
         [HttpGet]
         [Route("/[controller]/PrivacyPolicy")]
         public string GetAllPrivacyPolicyInfo()
@@ -348,14 +440,13 @@ namespace PraxisMapper.Controllers
                 "unmodified Praxismapper server. Data is used exclusively for gameplay purposes. You can request all data attached to your account for your viewing, and can request that data be deleted. If requested, the data " +
                 "will be decrypted before being provided, since it's your data and not a secret to you.<br />");
 
-            //TODO: server-specific addendum
+            //TODO: server-specific addendum. Probably to be added in the database somewhere.
 
-            //TODO: Plugin information.
+            //TODO: Plugin information. To be loaded as part of the IPraxisPlugin interface.
 
 
             return fullPolicy.ToString();
         }
-
     }
 }
 
