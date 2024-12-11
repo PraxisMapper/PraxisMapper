3f8407260300a7b422d5b67dd200102bb1af1e40
PraxisMapper <praxismapper@gmail.com>
2024-12-11 12:02:41 -0500

    Moved Data and SecureData controllers to their own plugin.

---
+++ PraxisClientDataPlugin/PraxisPlugin.cs
@@ -0,0 +1,28 @@
+﻿using PraxisCore;
+using PraxisCore.Support;
+
+namespace PraxisChatPlugin
+{
+    public class PraxisClientDataPlugin : IPraxisPlugin
+    {
+        public string PrivacyPolicy => "";
+        public string PluginName => "ClientData";
+        public float Version => 1.0f;
+        public List<DbTables.StyleEntry> Styles => new List<DbTables.StyleEntry>();
+
+        public void OnCreateAccount(string accountId)
+        {
+            return;
+        }
+
+        public void OnDeleteAccount(string accountId)
+        {
+            return;
+        }
+
+        public void Startup()
+        {
+            return;
+        }
+    }
+}
