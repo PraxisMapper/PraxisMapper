e7cf47839e79f0c7ae361faa0382ad5e4e5fb6a4
PraxisMapper <praxismapper@gmail.com>
2025-01-26 13:33:48 -0500

    Kestrel default port is now 5005? Quick attempt to document this

--- PraxisMapper/Program.cs
+++ PraxisMapper/Program.cs
@@ -15,7 +15,7 @@ namespace PraxisMapper {
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001");
+                    webBuilder.UseUrls("http://0.0.0.0:5005", "https://0.0.0.0:5001");
                 });
     }
 }
